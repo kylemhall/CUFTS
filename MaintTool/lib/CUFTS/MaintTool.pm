bff8040cfd5e992e9615191934d617ceca96fbe5
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2005-12-14 22:04:23 +0000

    Cleaned up code

--- MaintTool/lib/CUFTS/MaintTool.pm
+++ MaintTool/lib/CUFTS/MaintTool.pm
@@ -3,23 +3,22 @@ package CUFTS::MaintTool;
 use strict;
 use Catalyst qw/Session Session::Store::FastMmap Session::State::Cookie FormValidator CUFTS::MaintTool::FillInForm Static::Simple/;
 use lib '../lib';
+
 use CUFTS::Config;
 
 use Data::Dumper;
 
-
 our $VERSION = '2.00.00';
 
-CUFTS::MaintTool->config( 
-	name     => 'CUFTS::MaintTool',
-	default_display_per_page => 50,
+CUFTS::MaintTool->config(
+    name                     => 'CUFTS::MaintTool',
+    default_display_per_page => 50,
 );
 
-
 CUFTS::MaintTool->config->{session} = {
-	expires => 36000,
-	rewrite => 0,
-	storage => '/tmp/CUFTS_MaintTool_sessions',
+    expires => 36000,
+    rewrite => 0,
+    storage => '/tmp/CUFTS_MaintTool_sessions',
 };
 
 CUFTS::MaintTool->setup;
@@ -29,40 +28,46 @@ CUFTS::MaintTool->setup;
 ##
 
 sub begin : Private {
-	my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
 
-	# Set up basic template vars
-	
+    # Set up basic template vars
+
     $c->stash->{url_base} =
-        $c->config->{url_base}
+          $c->config->{url_base}
         ? $c->config->{url_base}
         : q{} . $c->req->base;
-        
-    $c->stash->{url_base} =~ s{/$}{};  # Remove trailing slash
 
-	# For a live setup, change these so that Catalyst isn't handling them.
+    $c->stash->{url_base} =~ s{/$}{};    # Remove trailing slash
 
-	$c->stash->{image_dir} = $c->stash->{url_base} . '/static/images/';
-	$c->stash->{css_dir}   = $c->stash->{url_base} . '/static/css/';
-	$c->stash->{js_dir}    = $c->stash->{url_base} . '/static/js/';
+    # For a live setup, change these so that Catalyst isn't handling them.
 
-	# don't force login on static content
-	return 1 if ($c->req->{path} =~ /^static/); 
+    $c->stash->{image_dir} = $c->stash->{url_base} . '/static/images/';
+    $c->stash->{css_dir}   = $c->stash->{url_base} . '/static/css/';
+    $c->stash->{js_dir}    = $c->stash->{url_base} . '/static/js/';
 
-	# Set up current user and site info in the stash
-			
-	if (defined($c->session->{current_account_id})) {
-		$c->stash->{current_account} = CUFTS::DB::Accounts->retrieve($c->session->{current_account_id});
+    # don't force login on static content
+    return 1 if ( $c->req->{path} =~ /^static/ );
 
-		defined($c->session->{current_site_id}) and
-			$c->stash->{current_site} = CUFTS::DB::Sites->retrieve($c->session->{current_site_id});
-		
-	} elsif ($c->req->action ne 'login') {
-		# If we have no current user then show the login screen
-		
-		$c->req->action(undef);
-		return $c->redirect('/login');
-	}
+    # Set up current user and site info in the stash
+
+    if ( defined( $c->session->{current_account_id} ) ) {
+        $c->stash->{current_account} = CUFTS::DB::Accounts->retrieve(
+            $c->session->{current_account_id} );
+
+        if ( defined( $c->session->{current_site_id} ) ) {
+            $c->stash->{current_site} = CUFTS::DB::Sites->retrieve( $c->session->{current_site_id} );
+        }
+
+    }
+    elsif ( $c->req->action ne 'login' ) {
+
+        # If we have no current user then show the login screen
+
+        $c->req->action(undef);
+        return $c->redirect('/login');
+    }
+    
+    return 1;
 }
 
 ##
@@ -70,7 +75,7 @@ sub begin : Private {
 ##
 
 sub end : Private {
-    my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
 
     return 1 if $c->response->status =~ /^3\d\d$/;
     return 1 if $c->response->body;
@@ -82,89 +87,93 @@ sub end : Private {
     $c->forward('CUFTS::MaintTool::V::TT');
 }
 
-
 ##
 ## login - Show the login screen
 ##
 
 sub login : Global {
-	my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
 
-	$c->form({'required' => ['login_key', 'login_password', 'submit'], 'filters' => ['trim']});
+    $c->form(
+        {   'required' => [ 'login_key', 'login_password', 'submit' ],
+            'filters'  => ['trim']
+        }
+    );
 
-	$c->session->{current_account_id} = undef;
-	$c->session->{current_site_id}    = undef;
-	$c->stash->{current_account}      = undef;
-	$c->stash->{current_site}         = undef;
+    $c->session->{current_account_id} = undef;
+    $c->session->{current_site_id}    = undef;
+    $c->stash->{current_account}      = undef;
+    $c->stash->{current_site}         = undef;
 
-	if (defined($c->form->{valid}->{login_key})) {
-		my ($key, $password) = ($c->form->{valid}->{login_key}, $c->form->{valid}->{login_password});
+    if ( defined( $c->form->{valid}->{login_key} ) ) {
+        my ( $key, $password ) = (
+            $c->form->{valid}->{login_key},
+            $c->form->{valid}->{login_password}
+        );
 
-		my $crypted_pass = crypt($password, $key);
-		my @accounts = CUFTS::DB::Accounts->search('key' => $key, 'password' => $crypted_pass);
-		
-		if (scalar(@accounts) == 1) {
-			$c->session->{current_account_id} = $accounts[0]->id;
-			
-			my @sites = $accounts[0]->sites;
-			if (scalar(@sites) == 1) {
-				$c->session->{current_site_id} = $sites[0]->id;
-			}
-			
-			$c->redirect('/main');
-		} else {
-			$c->stash->{errors} = ['The password or account was not recognized. Please check that you have entered the correct login name and password. If you are still having problems, please contact your CUFTS administrator.'];
-		}
-	} 
-	
-	$c->stash->{header_image} = 'login.jpg';
-	$c->stash->{template} = 'login.tt';
+        my $crypted_pass = crypt( $password, $key );
+        my @accounts = CUFTS::DB::Accounts->search(
+            'key'      => $key,
+            'password' => $crypted_pass
+        );
+
+        if ( scalar(@accounts) == 1 ) {
+            $c->session->{current_account_id} = $accounts[0]->id;
+
+            my @sites = $accounts[0]->sites;
+            if ( scalar(@sites) == 1 ) {
+                $c->session->{current_site_id} = $sites[0]->id;
+            }
+
+            $c->redirect('/main');
+        }
+        else {
+            $c->stash->{errors} = [
+                'The password or account was not recognized. Please check that you have entered the correct login name and password. If you are still having problems, please contact your CUFTS administrator.'
+            ];
+        }
+    }
+
+    $c->stash->{header_image} = 'login.jpg';
+    $c->stash->{template}     = 'login.tt';
 }
-
 
 ##
 ## logout - Remove the user session and forward to the login screen
-## 
+##
 
 sub logout : Global {
-	my ($self, $c) = @_;
-	$c->redirect('/login');
+    my ( $self, $c ) = @_;
+    $c->redirect('/login');
 }
 
 sub default : Private {
-    my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
     $c->redirect('/login');
-}	
-	
+}
 
 ##
 ## main - Display the main screen
 ##
 
 sub main : Global {
-	my ($self, $c) = @_;
-	
-	$c->stash->{template} = 'main.tt';
-	$c->stash->{header_image} = 'home.jpg';
+    my ( $self, $c ) = @_;
+
+    $c->stash->{template}     = 'main.tt';
+    $c->stash->{header_image} = 'home.jpg';
 }
-
-
 
 sub redirect {
-	my ($c, $location) = @_;
-	$location =~ m#^/# or
-		die("Attempting to redirect to relative location: $location");
+    my ( $c, $location ) = @_;
+    $location =~ m#^/#
+        or die("Attempting to redirect to relative location: $location");
 
-	if ($c->stash->{url_base}) {
-		$location = $c->stash->{url_base} . $location;
-	}
-	
-	$c->res->redirect($location);
+    if ( $c->stash->{url_base} ) {
+        $location = $c->stash->{url_base} . $location;
+    }
+
+    $c->res->redirect($location);
 }
-
-
-
-
 
 =head1 NAME
 
