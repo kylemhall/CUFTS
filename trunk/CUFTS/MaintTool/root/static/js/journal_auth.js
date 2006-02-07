413289fee585f85c2b9bdef9142837c760d99024
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-02-07 18:13:49 +0000

    Fixed bad URLs in bulk title templates Added journal_auth editing tools

--- trunk/CUFTS/MaintTool/root/static/js/journal_auth.js
+++ trunk/CUFTS/MaintTool/root/static/js/journal_auth.js
@@ -59,3 +59,55 @@ function clear_field (field_index, field_type, form_name) {
 
 	return false;
 }
+
+
+function add_edit_line (field_type) {
+	var newHTML;
+	var row = window['field_max'][field_type];
+	var div_class;
+	if (row == -1) {
+		div_class = 1;
+	} else {
+		div_class = (row + 1) % 2;
+	}
+
+	// Get the div object to append HTML to
+	var div = document.getElementById('journal-auth-edit-' + field_type);
+    newHTML = '<div class="row' + div_class + '"> ';
+
+	if (field_type == 'issns'){
+        newHTML = newHTML + '<input type="text" size="10" maxlength="9" name="new' + row + '_issn" value="" /> ';
+        newHTML = newHTML + '<input type="text" size="30" maxlength="512" name="new' + row + '_info" value="" />';
+	}
+    if (field_type == 'titles') {
+        newHTML = newHTML + '<input type="text" size="40" maxlength="1024" name="new' + row + '_title" value="" /> ';
+        newHTML = newHTML + '<input type="text" size="4" maxlength="3" name="new' + row + '_count" value="" />';
+	}
+
+    newHTML = newHTML + '</div>';
+	div.innerHTML = div.innerHTML + newHTML;
+
+	window['field_max'][field_type] += 1;
+
+	return false;
+}
+
+function show_edit_line (field_type) {
+    var row    = window['field_max'][field_type] + 1;
+    var div_id = 'new' + row + '_' + field_type;
+	var div    = document.getElementById(div_id);
+	
+	if (! div) {
+	    alert('Unable to show new ' + field_type + ' field.  Please save this record and try editting again.');
+	    return false;
+	}
+	
+	div.style.display = '';
+
+	window['field_max'][field_type] = row;
+
+	return false;
+}
+
+
+
