eb63435c3d4f8b564bf535af622fdee40bb55b87
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-02-27 18:13:21 +0000

    Removed CJDB DB settings which are no longer used

--- trunk/CUFTS/lib/CUFTS/AdvancedConfig.pm
+++ trunk/CUFTS/lib/CUFTS/AdvancedConfig.pm
@@ -43,10 +43,6 @@ use vars qw(
 
 	$CJDB_BASE_DIR
 
-	$CJDB_DB_ATTR
-	$CJDB_DB_STRING
-	@CJDB_DB_CONNECT
-
 	$CJDB_TEMPLATE_DIR
 	$CJDB_SITE_TEMPLATE_DIR
 
@@ -72,10 +68,6 @@ $CUFTS_MAIL_REPLY_TO = $CUFTS_MAIL_FROM;
 $CUFTS_REQUEST_LOG = "${CUFTS_LOG_DIR}/requests_log";
 
 $CUFTS_TEMPLATE_DIR = "${CUFTS_BASE_DIR}/templates";
-
-$CJDB_DB_STRING = "dbi:Pg:dbname=${CJDB_DB};host=localhost;port=5432";
-$CJDB_DB_ATTR = { 'PrintError' => 0, 'RaiseError' => 0, 'HandleError' => Exception::Class::DBI->handler() };
-@CJDB_DB_CONNECT = ($CJDB_DB_STRING, $CJDB_USER, $CJDB_PASSWORD, $CJDB_DB_ATTR);
 
 $CJDB_BASE_DIR = "$CUFTS_BASE_DIR/CJDB";
 
