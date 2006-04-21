7ea1e8baf24d950507150e0a1520db0094124780
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-04-21 21:24:26 +0000

    More trimming options (without breaking existing trims, hopefully

--- trunk/CUFTS/lib/CUFTS/Util/Simple.pm
+++ trunk/CUFTS/lib/CUFTS/Util/Simple.pm
@@ -21,25 +21,40 @@ sub not_empty_string :Export( :DEFAULT ) {
 }
 
 sub ltrim_string :Export( :DEFAULT ) {
-    my ($string) = @_;
+    my ($string, $trim) = @_;
     return undef if !defined($string);
-    $string =~ s/^ [\n\s]+ //xsm;
+    if (!defined($trim) ) {
+        $trim = '';
+    }
+    $string =~ s/^ [\n\s]* $trim? [\n\s]* //xsm;
     return $string;
 }
 
 sub rtrim_string :Export( :DEFAULT ) {
-    my ($string) = @_;
+    my ($string, $trim) = @_;
     return undef if !defined($string);
-    $string =~ s/ [\n\s]+ $//xsm;
+    if (!defined($trim) ) {
+        $trim = '';
+    }
+    $string =~ s/ [\n\s]* $trim? [\n\s]* $//xsm;
     return $string;    
 }
 
 sub trim_string :Export( :DEFAULT ) {
+    my ($string, $trim) = @_;
+    return undef if !defined($string);
+    $string = ltrim_string($string, $trim);
+    $string = rtrim_string($string, $trim);
+    return $string;
+}
+
+sub dashed_issn :Export( :DEFAULT ) {
     my ($string) = @_;
     return undef if !defined($string);
-    $string =~ s/^ [\n\s]+  //xsm;
-    $string =~ s/  [\n\s]+ $//xsm;
-    return $string;    
+    if ( length($string) == 8 ) {
+        $string = substr( $string, 0, 4 ) . '-' . substr( $string, 4, 4 )
+    }
+    return $string;
 }
 
 1;
