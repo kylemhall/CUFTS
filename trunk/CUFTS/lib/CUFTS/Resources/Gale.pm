c37e51f739c6c9c502f15c12175efeea2a084228
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-05-01 22:43:14 +0000

    Removed proxy_prefix which was no longer being used

--- trunk/CUFTS/lib/CUFTS/Resources/Gale.pm
+++ trunk/CUFTS/lib/CUFTS/Resources/Gale.pm
@@ -8,7 +8,7 @@
 ## the terms of the GNU General Public License as published by the Free
 ## Software Foundation; either version 2 of the License, or (at your option)
 ## any later version.
-## 
+##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
 ## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -22,207 +22,207 @@ package CUFTS::Resources::Gale;
 
 use base qw(CUFTS::Resources::Base::Journals);
 
-use CUFTS::Exceptions qw(assert_ne);
+use CUFTS::Exceptions;
+use CUFTS::Util::Simple;
 
 use strict;
 
 sub title_list_fields {
-	return [qw(
-		id
-		title
-		issn
-		ft_start_date
-		ft_end_date
-		cit_start_date
-		cit_end_date
+    return [
+        qw(
+            id
+            title
+            issn
+            ft_start_date
+            ft_end_date
+            cit_start_date
+            cit_end_date
 
-		journal_url
-	)];
+            journal_url
+        )
+    ];
 }
 
 sub title_list_field_map {
-	return {
-		'title'			=> 'title',
-		'issn'			=> 'issn',
-		'citation_start'	=> 'cit_start_date',
-		'citation_end'		=> 'cit_end_date',
-		'fulltext_start'	=> 'ft_start_date',
-		'fulltext_end'		=> 'ft_end_date',
-		'urlbase'		=> 'journal_url',
-	};
+    return {
+        'title'          => 'title',
+        'issn'           => 'issn',
+        'citation_start' => 'cit_start_date',
+        'citation_end'   => 'cit_end_date',
+        'fulltext_start' => 'ft_start_date',
+        'fulltext_end'   => 'ft_end_date',
+        'urlbase'        => 'journal_url',
+    };
 }
 
 sub clean_data {
-	my ($class, $record) = @_;
-	my @errors;
+    my ( $class, $record ) = @_;
+    my @errors;
 
-	$record->{'title'} =~ s/\s*\(.+?\)$//;
-	
-	return 	$class->SUPER::clean_data($record);
+    $record->{'title'} =~ s/\s*\(.+?\)\s*$//;
+
+    return $class->SUPER::clean_data($record);
 }
 
-
 sub global_resource_details {
-	my ($class) = @_;
-	return [ @{$class->SUPER::global_resource_details},
-		 qw(
-		 	url_base
-		 )
-	];
+    my ($class) = @_;
+    return [
+        @{ $class->SUPER::global_resource_details },
+        qw(
+            url_base
+        )
+    ];
 }
 
 sub local_resource_details {
-	my ($class) = @_;
-	return [ @{$class->SUPER::local_resource_details},
-		 qw(
-		 	proxy_suffix
-		 	proxy_prefix
-		 )
-	];
+    my ($class) = @_;
+    return [
+        @{ $class->SUPER::local_resource_details },
+        qw(
+            proxy_suffix
+        )
+    ];
 }
 
 sub resource_details_help {
-	return {
-		$_[0]->SUPER::resource_details_help,
-		'url_base' => "Base URL for faking searches.\nExample:\nhttp://infotrac.galegroup.com/itw/infomark/1/1/1/purl=rc11_CPI_0_",
-	}
+    return { $_[0]->SUPER::resource_details_help,
+        'url_base' =>
+            "Base URL for faking searches.\nExample:\nhttp://infotrac.galegroup.com/itw/infomark/1/1/1/purl=rc11_CPI_0_",
+    };
 }
 
 sub overridable_resource_details {
-	return undef;
+    return undef;
 }
-	
 
 sub build_linkJournal {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkJournal');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkJournal');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	my @results;
-	foreach my $record (@$records) {
-		next unless assert_ne($record->journal_url);
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkJournal');
 
-		my $url = $record->journal_url;
+    my @results;
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->journal_url );
 
-		# Workaround for GODOT weirdness!
-		
-#		$url = 'http://stalefish.lib.sfu.ca:7331/godot/redirector.cgi?_url_=' . $url;
+        my $url = $record->journal_url;
 
-		my $result = new CUFTS::Result($url);
-		$result->record($record);
-		push @results, $result;
-	}
+        if ( defined( $resource->proxy_suffix ) ) {
+            $url .= $resource->proxy_suffix;
+        }
 
-	return \@results;
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+        push @results, $result;
+    }
+
+    return \@results;
 }
 
 # Fakes a search like this:
 # http://infotrac.galegroup.com/itw/infomark/1/1/1/purl=rc11_LT_0_sn_0891-6330_AND_vo_16_AND_iu_4
 
 sub build_linkTOC {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkTOC');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkTOC');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	defined($resource->url_base) or
-		CUFTS::Exception::App->throw('No url_base defined for resource.');
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkTOC');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkTOC');
 
-	my @results;
-	foreach my $record (@$records) {
-		next unless assert_ne($record->issn);
-		next unless assert_ne($request->volume) || assert_ne($request->issue);
+    defined( $resource->url_base )
+        or CUFTS::Exception::App->throw('No url_base defined for resource.');
 
-		my $issn = $record->issn;
-		substr($issn, 4, 0) = '-';
+    my @results;
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->issn );
 
-		my $url = $resource->url_base;
-		$url .= "sn_$issn";
+        next if is_empty_string( $request->volume )
+             && is_empty_string( $request->issue  );
 
-		assert_ne($request->volume) and
-			$url .= '_AND_vo_' . $request->volume;
+        my $issn = $record->issn;
+        substr( $issn, 4, 0 ) = '-';
 
-		assert_ne($request->issue) and
-			$url .= '_AND_iu_' . $request->issue;
+        my $url = $resource->url_base;
+        $url .= "sn_$issn";
 
-		# Workaround for GODOT weirdness!
-		
-#		$url = 'http://stalefish.lib.sfu.ca:7331/godot/redirector.cgi?_url_=' . $url;
+        if ( not_empty_string($request->volume) ) {
+            $url .= '_AND_vo_' . $request->volume;
+        }
 
-		my $result = new CUFTS::Result($url);
-		$result->record($record);
-		push @results, $result;
-	}
+        if ( not_empty_string($request->issue) ) {
+            $url .= '_AND_iu_' . $request->issue;
+        }
 
-	return \@results;
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+        push @results, $result;
+    }
+
+    return \@results;
 }
-
 
 # Fakes a search like this:
 # http://infotrac.galegroup.com/itw/infomark/1/1/1/purl=rc11_LT_0_sn_0891-6330_AND_vo_16_AND_iu_4
 
 sub build_linkFulltext {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkTOC');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkTOC');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	defined($resource->url_base) or
-		CUFTS::Exception::App->throw('No url_base defined for resource.');
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkTOC');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkTOC');
 
-	my @results;
-	foreach my $record (@$records) {
-		next unless assert_ne($record->issn);
-		next unless assert_ne($request->volume) || assert_ne($request->issue);
+    defined( $resource->url_base )
+        or CUFTS::Exception::App->throw('No url_base defined for resource.');
 
-		my $issn = $record->issn;
-		substr($issn, 4, 0) = '-';
+    my @results;
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->issn );
 
-		my $url = $resource->url_base;
+        next if is_empty_string( $request->volume )
+             && is_empty_string( $request->issue  );
 
-		$url .= 'ke_';
+        my $url = $resource->url_base;
 
-		$url .= "sn+$issn";
+        $url .= 'ke_sn+' . dashed_issn( $record->issn );
 
-		assert_ne($request->volume) and
-			$url .= '+AND+vo+' . $request->volume;
+        if ( not_empty_string($request->volume) ) {
+            $url .= '+AND+vo+' . $request->volume;
+        }
+    
+        if ( not_empty_string($request->issue) ) {
+            $url .= '+AND+iu+' . $request->issue;
+        }
 
-		assert_ne($request->issue) and
-			$url .= '+AND+iu+' . $request->issue;
-			
-		assert_ne($request->spage) and
-			$url .= '+AND+sp+' . $request->spage;
+        if ( not_empty_string($request->spage) ) {
+            $url .= '+AND+sp+' . $request->spage;
+        }
 
-#		assert_ne($request->atitle) and
-#			$url .= '_AND_ti_' . $request->atitle;
+#        if ( not_empty_string($request->atitle) ) { 
+#            $url .= '+AND+ti+' . $request->atitle;
+#        }
 
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+        push @results, $result;
+    }
 
-		my $result = new CUFTS::Result($url);
-		$result->record($record);
-		push @results, $result;
-	}
-
-	return \@results;
+    return \@results;
 }
-
 
 1;
