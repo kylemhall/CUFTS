ae894c24aeb3c75cf3e6ffcf71929b3942a89a96
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-05-01 20:00:53 +0000

    Fixed SQL typo

--- trunk/CUFTS/lib/CJDB/DB/Journals.pm
+++ trunk/CUFTS/lib/CJDB/DB/Journals.pm
@@ -189,7 +189,7 @@ sub search_distinct_by_tags {
 			$search_sql .= ' AND (cjdb_tags.viewing = ? OR (cjdb_tags.viewing = ? AND cjdb_tags.site = ?)) ';
 			push @bind, 1, 2, $site;
 		} elsif ($viewing == 4) {
-    		$search_sql .= ' AND (cjdb_tags.viewing = ? OR cjdb_tags.viewing = ?) AND cjdb_tags.site = ?) ';
+    		$search_sql .= ' AND (cjdb_tags.viewing = ? OR cjdb_tags.viewing = ?) AND cjdb_tags.site = ? ';
     		push @bind, 1, 2, $site;
     	}
 
