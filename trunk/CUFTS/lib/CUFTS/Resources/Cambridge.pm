edcc78c52680cbbed2dd681c24c81197f2df1dc4
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-05-04 22:36:12 +0000

    Changed loader to match new title list format

--- trunk/CUFTS/lib/CUFTS/Resources/Cambridge.pm
+++ trunk/CUFTS/lib/CUFTS/Resources/Cambridge.pm
@@ -8,7 +8,7 @@
 ## the terms of the GNU General Public License as published by the Free
 ## Software Foundation; either version 2 of the License, or (at your option)
 ## any later version.
-## 
+##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
 ## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -22,7 +22,8 @@ package CUFTS::Resources::Cambridge;
 
 use base qw(CUFTS::Resources::Base::DOI CUFTS::Resources::Base::Journals);
 
-use CUFTS::Exceptions qw(assert_ne);
+use CUFTS::Exceptions;
+use CUFTS::Util::Simple;
 
 use URI::Escape;
 
@@ -35,249 +36,284 @@ my $url_base = 'http://journals.cambridge.org/';
 ##
 
 sub title_list_fields {
-	return [qw(
-		id
-		title
-		issn
-		e_issn
-		ft_start_date
-		ft_end_date
-		vol_ft_start
-		iss_ft_start
-		vol_ft_end
-		iss_ft_end
-		db_identifier
-	)];
-}
-
-
-## title_list_field_map - Hash ref mapping fields from the raw title lists to
-## internal field names
-##
-
-sub title_list_field_map {
-	return {
-		'Title' 		=> 'title',
-		'ISSN' 			=> 'issn',
-		'Electronic ISSN'	=> 'e_issn',
-		'ft_start_date' 	=> 'ft_start_date',
-		'ft_end_date'		=> 'ft_end_date',
-		'Start volume'		=> 'vol_ft_start',
-		'End volume'		=> 'vol_ft_end',
-		'Start issue'		=> 'iss_ft_start',
-		'End Issue'		=> 'iss_ft_end',
-		'Mnemonic'		=> 'db_identifier'
-	};
+    return [
+        qw(
+            id
+            title
+            issn
+            e_issn
+            ft_start_date
+            ft_end_date
+            vol_ft_start
+            vol_ft_end
+            db_identifier
+        )
+    ];
 }
 
 sub clean_data {
-	my ($class, $record) = @_;
-	
-	foreach my $field (qw(vol_ft_start iss_ft_start vol_ft_end iss_ft_end)) {
-		defined($record->{$field}) && int($record->{$field}) < 0 and
-			delete $record->{$field};
-	}
-	
-	return $class->SUPER::clean_data($record);
-}
+    my ( $class, $record ) = @_;
 
+    foreach my $field (qw(vol_ft_start  vol_ft_end)) {
+        if ( defined( $record->{$field} ) && int( $record->{$field} ) < 0 ) {
+            delete $record->{$field};
+        }
+    }
+    
+    if ( $record->{issn} !~ / \d{4} -? \d{3}[\dxX] /xsm ) {
+        delete $record->{issn};
+    }
+
+    if ( $record->{e_issn} !~ / \d{4} -? \d{3}[\dxX] /xsm ) {
+        delete $record->{e_issn};
+    }
+
+    return $class->SUPER::clean_data($record);
+}
 
 ## preprocess_file - Join the multi-line style of title list into one title list
 ##                   write a temp file and open it.
 
 sub preprocess_file {
-	my ($class, $IN) = @_;
+    my ( $class, $IN ) = @_;
 
-	use File::Temp;
-	
-	my ($fh, $filename) = File::Temp::tempfile();
-	
-	# Grab header row
-	my $header = <$IN>;
-	my @in_header = split /\t/, $header;
-	my @out_header = @in_header;
-	
-	# Build column map
-	
-	my %columns;
-	foreach my $x (0..$#in_header) {
-		$columns{$in_header[$x]} = $x;
-	}
+    use File::Temp;
 
-	splice @out_header, $columns{'Year'}, 1, ('ft_start_date', 'ft_end_date');
+    my ( $fh, $filename ) = File::Temp::tempfile();
 
-	print $fh (join "\t", @out_header);
-		
-	# For now, assume the file has Mneumonic first, year second and vol(iss) third;
+    # Grab header row
 
-	my $current_id;
-	my $first_vol;
-	my $first_iss;
-	my $first_year;
-	my @prev_fields;
-	
-	while (my $line = <$IN>) {
-		my @fields = split /\t/, $line;
-		if ($current_id ne $fields[0]) {
-			if (defined($current_id)) {
-				$prev_fields[$columns{'Start volume'}] = $first_vol;
-				$prev_fields[$columns{'Start issue'}] = $first_iss;
-				splice @prev_fields, $columns{'Year'}, 0, $first_year;
-				print $fh (join "\t", @prev_fields);
-			}
-			
-			$current_id = $fields[$columns{'Mneumonic'}];
-			$first_vol = $fields[$columns{'Start volume'}];
-			$first_iss = $fields[$columns{'Start issue'}];
-			$first_year = $fields[$columns{'Year'}];			
+    my $header    = <$IN>;
+    my @in_header = split /\t/, $header;
 
-		}
+    # Build column map
 
-		@prev_fields = @fields;
-	}	
+    my %columns;
+    foreach my $x ( 0 .. $#in_header ) {
+        $columns{ $in_header[$x] } = $x;
+    }
+    
+    my %data;
 
-	splice @prev_fields, ($columns{'Year'} + 1), 0, $first_year;
-	$prev_fields[$columns{'Start volume'}] = $first_vol;
-	$prev_fields[$columns{'Start issue'}] = $first_iss;
-	print $fh (join "\t", @prev_fields);
+    while ( my $line = <$IN> ) {
 
+        my @fields = split /\t/, $line;
 
-	close *$IN;
-	seek *$fh, 0, 0;
+        my $journal_id = $fields[ $columns{'JOURNAL_ID'} ];
+        my $title      = $fields[ $columns{'TITLE'} ];
+        my $year       = $fields[ $columns{'YEAR'} ];
+        my $issn       = $fields[ $columns{'OFFLINE_ISSN'} ];
+        my $e_issn     = $fields[ $columns{'ONLINE_ISSN'} ];
+        my $volumes    = $fields[ $columns{'VOLUMES'} ];
 
-	return $fh;
+        next if $year !~ / \d{4} /xsm;
+
+        if ( $volumes =~ / V(\d+) /xsm ) {
+            $volumes = $1;
+        }
+        else {
+            next;
+        }
+
+        if ( !exists( $data{$journal_id} ) ) {
+            $data{$journal_id}->{journal_id}    = $journal_id;
+            $data{$journal_id}->{title}         = $title;
+            $data{$journal_id}->{issn}          = $issn;
+            $data{$journal_id}->{e_issn}        = $e_issn;
+            $data{$journal_id}->{ft_start_date} = $year;
+            $data{$journal_id}->{ft_end_date}   = $year;
+            $data{$journal_id}->{vol_ft_start}  = $volumes;
+            $data{$journal_id}->{vol_ft_end}    = $volumes;
+        }
+        else {
+            if ( $year < $data{$journal_id}->{ft_start_date} ) {
+                $data{$journal_id}->{ft_start_date} = $year;
+            }
+
+            if ( $year > $data{$journal_id}->{ft_end_date} ) {
+                $data{$journal_id}->{ft_end_date} = $year;
+            }
+            
+            if ( $volumes < $data{$journal_id}->{vol_ft_start} ) {
+                $data{$journal_id}->{vol_ft_start} = $volumes;
+            }
+
+            if ( $volumes > $data{$journal_id}->{vol_ft_end} ) {
+                $data{$journal_id}->{vol_ft_end} = $volumes;
+            }
+        }
+    }
+    
+    print $fh (
+        join "\t", qw(
+            title
+            issn
+            e_issn
+            ft_start_date
+            ft_end_date
+            vol_ft_start
+            vol_ft_end
+            db_identifier
+        )
+    );
+    print $fh "\n";
+
+    foreach my $rec ( values(%data) ) {
+
+        print $fh (
+            join "\t", (
+                $rec->{title},
+                $rec->{issn},
+                $rec->{e_issn},
+                $rec->{ft_start_date},
+                $rec->{ft_end_date},
+                $rec->{vol_ft_start},
+                $rec->{vol_ft_end},
+                $rec->{journal_id}
+            )
+        );
+        print $fh "\n";
+        
+    }
+
+    close *$IN;
+    seek *$fh, 0, 0;
+
+    return $fh;
 }
-
-
-
-
-
-
 
 ## -------------------------------------------------------------------------------------------
-                
-## can_get* - Control whether or not an attempt to create a link is built.  This is run  
+
+## can_get* - Control whether or not an attempt to create a link is built.  This is run
 ## before the database is searched for possible title matches, so catching requests without
 ## enough data, etc. early (here) cuts down on database hits
-                                
-                
+
 sub can_getTOC {
-        my ($class, $request) = @_;
-                
-        return 0 unless (assert_ne($request->volume) && assert_ne($request->issue));
-        
-        return $class->SUPER::can_getTOC($request);
+    my ( $class, $request ) = @_;
+
+    return 0
+        if is_empty_string( $request->volume )
+        || is_empty_string( $request->issue );
+
+    return $class->SUPER::can_getTOC($request);
 }
-                
+
 # --------------------------------------------------------------------------------------------
-	
+
 ## build_link* - Builds a link to a service.  Should return an array reference containing
 ## Result objects with urls and title list records (if applicable).
 ##
 
-
 sub build_linkTOC {
-        my ($class, $records, $resource, $site, $request) = @_;
-                 
-        defined($records) && scalar(@$records) > 0 or
-                return [];
-        defined($resource) or
-                CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
-        defined($site) or
-                CUFTS::Exception::App->throw('No site defined in build_linkTOC');
-        defined($request) or
-                CUFTS::Exception::App->throw('No request defined in build_linkTOC');
-	                
-        my @results;
-        
-        
-        foreach my $record (@$records) {
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-                my $url = $url_base . 'issue_' . &prepTitle($record->title);
-		$url .= '/Vol' . sprintf("%02u", $request->volume) . 'No' . sprintf("%02u", $request->issue);
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or
+        CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkTOC');
+    defined($request)
+        or
+        CUFTS::Exception::App->throw('No request defined in build_linkTOC');
 
-                my $result = new CUFTS::Result($url);
-                $result->record($record);
-        
-                push @results, $result;
-        }       
-        
-        return \@results;
-}       
+    my @results;
+
+    foreach my $record (@$records) {
+
+        my $url = $url_base . 'issue_' . prepTitle( $record->title );
+        $url .= '/Vol'
+            . sprintf( "%02u", $request->volume ) . 'No'
+            . sprintf( "%02u", $request->issue );
+
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
+}
 
 #### ROUTINE TO FORMAT JOURNAL TITLE FOR TABLE OF CONTENTS LINK
 sub prepTitle {
 
-	my $title = shift;
-	
-	my $preppedTitle = "";
+    my $title = shift;
 
-	# remove spaces and upper case first letter of each word in title
-	my @words = split(/\s/, $title);
-	foreach my $word(@words){
-		$preppedTitle .= ucfirst($word);
-	}
-	$preppedTitle =~  s/,//g;
-	$preppedTitle = uri_escape($preppedTitle);
-	
-	return $preppedTitle;
+    my $preppedTitle = "";
+
+    # remove spaces and upper case first letter of each word in title
+    my @words = split( /\s/, $title );
+    foreach my $word (@words) {
+        $preppedTitle .= ucfirst($word);
+    }
+    $preppedTitle =~ s/,//g;
+    $preppedTitle = uri_escape($preppedTitle);
+
+    return $preppedTitle;
 }
 
 sub build_linkJournal {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkJournal');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkJournal');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	my @results;
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw(
+        'No resource defined in build_linkJournal');
+    defined($site)
+        or
+        CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw(
+        'No request defined in build_linkJournal');
 
-	foreach my $record (@$records) {
+    my @results;
 
-		next unless assert_ne($record->db_identifier);
+    foreach my $record (@$records) {
 
-                my $url = $url_base . 'jid_' . $record->db_identifier;
+        next if is_empty_string( $record->db_identifier );
 
-		my $result = new CUFTS::Result($url);
-		$result->record($record);
-		
-		push @results, $result;
-	}
+        my $url = $url_base . 'jid_' . $record->db_identifier;
 
-	return \@results;
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
 }
 
 sub build_linkDatabase {
-        my ($class, $records, $resource, $site, $request) = @_;
-  
-        defined($records) && scalar(@$records) > 0 or
-                return [];
-        defined($resource) or
-                CUFTS::Exception::App->throw('No resource defined in build_linkDatabase');
-        defined($site) or
-                CUFTS::Exception::App->throw('No site defined in build_linkDatabase');
-        defined($request) or
-                CUFTS::Exception::App->throw('No request defined in build_linkDatabase');
-        
-        my @results;
-        
-        foreach my $record (@$records) {
-                  
-		my $url = 'http://www.journals.cup.org/';
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-                my $result = new CUFTS::Result($url);
-                $result->record($record);
-           
-                push @results, $result;
-        }
-  
-        return \@results;
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw(
+        'No resource defined in build_linkDatabase');
+    defined($site)
+        or
+        CUFTS::Exception::App->throw('No site defined in build_linkDatabase');
+    defined($request)
+        or CUFTS::Exception::App->throw(
+        'No request defined in build_linkDatabase');
+
+    my @results;
+
+    foreach my $record (@$records) {
+
+        my $url = $resource->database_url || 'http://www.journals.cup.org/';
+
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
 }
-
 
 1;
