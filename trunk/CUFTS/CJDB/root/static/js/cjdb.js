cbbfb979337d0c9d92025baf1467cbb559b38b5c
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-05-18 16:08:24 +0000

    Added support for tab memory

--- trunk/CUFTS/CJDB/root/static/js/cjdb.js
+++ trunk/CUFTS/CJDB/root/static/js/cjdb.js
@@ -1,3 +1,33 @@
+function createCookie(name,value,days)
+{
+	if (days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name)
+{
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name)
+{
+	createCookie(name,"",-1);
+}
+
 function imageSwap(image, image1, image2) {
 	var currentSrc = document[image].src;
 	imgregex = new RegExp(image1 + '$')	
@@ -116,7 +146,15 @@ function changeBrowseAlt(activate) {
    
    var fields = Form.getInputs("form-" + activate, "text", "search_terms");
    Field.activate(fields[0]);
+
+   createCookie('cjdb_activate_tab', activate);
    
    return false;
 }
 
+function rememberTab() {
+    activate = readCookie('cjdb_activate_tab');
+    if (activate) {
+        changeBrowseAlt(activate);
+    }
+}
