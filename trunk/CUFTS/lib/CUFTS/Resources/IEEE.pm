2f7cc8e61fff635d9703753272ae5876ff36d791
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-05-31 22:42:57 +0000

    Changed to allow direct upload of tab delimited fil

--- trunk/CUFTS/lib/CUFTS/Resources/IEEE.pm
+++ trunk/CUFTS/lib/CUFTS/Resources/IEEE.pm
@@ -8,7 +8,7 @@
 ## the terms of the GNU General Public License as published by the Free
 ## Software Foundation; either version 2 of the License, or (at your option)
 ## any later version.
-## 
+##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
 ## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -22,7 +22,8 @@ package CUFTS::Resources::IEEE;
 
 use base qw(CUFTS::Resources::Base::Journals);
 
-use CUFTS::Exceptions qw(assert_ne);
+use CUFTS::Exceptions;
+use CUFTS::Util::Simple;
 
 use strict;
 
@@ -30,139 +31,147 @@ my $url_base = 'http://ieeexplore.ieee.org/';
 
 ## title_list_fields - Controls what fields get displayed and loaded from
 ## title lists.
-##
 
 sub title_list_fields {
-	return [qw(
-		id
-		title
-		issn
-		ft_start_date
-		ft_end_date
-		db_identifier
-	)];
+    return [
+        qw(
+            id
+            title
+            issn
+            ft_start_date
+            ft_end_date
+            db_identifier
+        )
+    ];
 }
-
 
 ## title_list_field_map - Hash ref mapping fields from the raw title lists to
 ## internal field names
 ##
 
 sub title_list_field_map {
-	return {
-		'title' 		=> 'title',
-		'issn' 			=> 'issn',
-		'ft_start_date' 	=> 'ft_start_date',
-		'ft_end_date'		=> 'ft_end_date',
-		'db_identifier'		=> 'db_identifier'
-	};
+    return {
+        'TITLE'              => 'title',
+        'ISSN#'              => 'issn',
+        'START YEAR'         => 'ft_start_date',
+        'END YEAR'           => 'ft_end_date',
+        'PUBLICATION NUMBER' => 'db_identifier'
+    };
+}
+
+sub title_list_extra_requires {
+    require Text::CSV;
+}
+
+sub title_list_split_row {
+    my ( $class, $row ) = @_;
+
+    my $csv = Text::CSV->new();
+    $csv->parse($row)
+        or CUFTS::Exception::App->throw('Error parsing CSV line: ' . $csv->error_input() );
+
+    my @fields = $csv->fields;
+    return \@fields;
+}
+
+sub clean_data {
+    my ( $class, $record ) = @_;
+    
+    if ( $record->{ft_start_date} !~ /\d{4}/ ) {
+        delete $record->{ft_start_date};
+    }
+
+    if ( $record->{ft_end_date} !~ /\d{4}/ ) {
+        delete $record->{ft_end_date};
+    }
+
+    if ( $record->{issn} !~ / \d{4} - \d{3}[\dxX] /xsm ) {
+        delete $record->{issn};
+    }
+
+    # Remove [ see also... ]
+    
+    $record->{title} =~ s/ \s+ \[ .+ \] \s* //xsm;
+    
+    return $class->SUPER::clean_data($record);
 }
 
 ## -------------------------------------------------------------------------------------------
-                
-## can_get* - Control whether or not an attempt to create a link is built.  This is run  
+
+## can_get* - Control whether or not an attempt to create a link is built.  This is run
 ## before the database is searched for possible title matches, so catching requests without
 ## enough data, etc. early (here) cuts down on database hits
-                                
-                
+
 sub can_getTOC {
-        my ($class, $request) = @_;
-                
-        return 0 unless (assert_ne($request->volume) && assert_ne($request->issue));
-        
-        return $class->SUPER::can_getTOC($request);
+    my ( $class, $request ) = @_;
+
+    return 0
+        if is_empty_string( $request->volume )
+        || is_empty_string( $request->issue  );
+
+    return $class->SUPER::can_getTOC($request);
 }
-                
+
 # --------------------------------------------------------------------------------------------
-	
+
 ## build_link* - Builds a link to a service.  Should return an array reference containing
 ## Result objects with urls and title list records (if applicable).
 ##
 
-
 sub build_linkTOC {
-        my ($class, $records, $resource, $site, $request) = @_;
-                 
-        defined($records) && scalar(@$records) > 0 or
-                return [];
-        defined($resource) or
-                CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
-        defined($site) or
-                CUFTS::Exception::App->throw('No site defined in build_linkTOC');
-        defined($request) or
-                CUFTS::Exception::App->throw('No request defined in build_linkTOC');
-                
-        my @results;
-        
-        
-        foreach my $record (@$records) {
-                next unless assert_ne($record->db_identifier);
-                
-		my $url = $url_base . 'servlet/opac?punumber=' . $record->db_identifier;
-		$url .= '&isvol=' . $request->volume . '&isno=' . $request->issue;
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-                my $result = new CUFTS::Result($url);
-                $result->record($record);
-        
-                push @results, $result;
-        }       
-        
-        return \@results;
-}       
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkTOC');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkTOC');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkTOC');
+
+    my @results;
+
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->db_identifier );
+
+        my $url = $url_base . 'servlet/opac?punumber=' . $record->db_identifier;
+        $url .= '&isvol=' . $request->volume . '&isno=' . $request->issue;
+
+        my $result = new CUFTS::Result($url);
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
+}
 
 sub build_linkJournal {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkJournal');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkJournal');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	my @results;
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkJournal');
 
-	foreach my $record (@$records) {
+    my @results;
 
-		next unless assert_ne($record->db_identifier);
+    foreach my $record (@$records) {
 
-		my $result = new CUFTS::Result($url_base . 'servlet/opac?punumber=' . $record->db_identifier);
-		$result->record($record);
-		
-		push @results, $result;
-	}
+        next if is_empty_string( $record->db_identifier );
 
-	return \@results;
+        my $result = new CUFTS::Result($url_base . 'servlet/opac?punumber=' . $record->db_identifier );
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
 }
-
-sub build_linkDatabase {
-        my ($class, $records, $resource, $site, $request) = @_;
-  
-        defined($records) && scalar(@$records) > 0 or
-                return [];
-        defined($resource) or
-                CUFTS::Exception::App->throw('No resource defined in build_linkDatabase');
-        defined($site) or
-                CUFTS::Exception::App->throw('No site defined in build_linkDatabase');
-        defined($request) or
-                CUFTS::Exception::App->throw('No request defined in build_linkDatabase');
-        
-        my @results;
-        
-        foreach my $record (@$records) {
-                
- 		next unless assert_ne($resource->database_url);
-  
-                my $result = new CUFTS::Result($resource->database_url);
-                $result->record($record);
-           
-                push @results, $result;
-        }
-  
-        return \@results;
-}
-
 
 1;
