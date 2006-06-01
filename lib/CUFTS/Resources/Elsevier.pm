53d4c1a226977c7266dd3f2c8c0e26c34c87c2a4
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-06-01 17:53:48 +0000

    New style

--- lib/CUFTS/Resources/Elsevier.pm
+++ lib/CUFTS/Resources/Elsevier.pm
@@ -8,7 +8,7 @@
 ## the terms of the GNU General Public License as published by the Free
 ## Software Foundation; either version 2 of the License, or (at your option)
 ## any later version.
-## 
+##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
 ## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -22,168 +22,173 @@ package CUFTS::Resources::Elsevier;
 
 use base qw(CUFTS::Resources::Base::DOI CUFTS::Resources::Base::Journals);
 
-use CUFTS::Exceptions qw(assert_ne);
+use CUFTS::Exceptions;
+use CUFTS::Util::Simple;
 
 use strict;
 
 sub title_list_fields {
-	return [qw(
-		id
-		title
-		issn
-		ft_start_date
-		vol_ft_start
-		iss_ft_start
-		ft_end_date
-		vol_ft_end
-		iss_ft_end
-		journal_url
-		publisher
-		current_months
-	)];
+    return [
+        qw(
+            id
+            title
+            issn
+            ft_start_date
+            vol_ft_start
+            iss_ft_start
+            ft_end_date
+            vol_ft_end
+            iss_ft_end
+            journal_url
+            publisher
+            current_months
+        )
+    ];
 }
 
-
-
 sub help_template {
-	return 'help/Elsevier';
+    return 'help/Elsevier';
 }
 
 sub resource_details_help {
-	return {
-	}
+    return {};
 }
 
-
 sub title_list_field_map {
-	return {
-		'Publication Title'	=> 'title',
-		'ISSN' 			=> 'issn',
-		'Doc URL'		=> 'journal_url',
-		'Publisher'		=> 'publisher',
-	};
+    return {
+        'Publication Title' => 'title',
+        'ISSN'              => 'issn',
+        'Doc URL'           => 'journal_url',
+        'Publisher'         => 'publisher',
+    };
 }
 
 sub title_list_extra_requires {
-	require Text::CSV;
+    require Text::CSV;
 }
 
 sub title_list_split_row {
-	my ($class, $row) = @_;
-	
-	my $csv = Text::CSV->new();
-	$csv->parse($row) or
-		CUFTS::Exception::App->throw('Error parsing CSV line: ' . $csv->error_input());
-	
-	my @fields = $csv->fields;
-	return \@fields;
+    my ( $class, $row ) = @_;
+
+    my $csv = Text::CSV->new();
+    $csv->parse($row)
+        or CUFTS::Exception::App->throw('Error parsing CSV line: ' . $csv->error_input() );
+
+    my @fields = $csv->fields;
+    return \@fields;
 }
 
 sub clean_data {
-	my ($class, $record) = @_;
+    my ( $class, $record ) = @_;
 
-	if (defined($record->{'___Coverage Begins'})) {
-		if ($record->{'___Coverage Begins'} =~ /vol[^0-9]+([0-9]+)/i) {
-			$record->{'vol_ft_start'} = $1;
-		}
-		if ($record->{'___Coverage Begins'} =~ /iss[^0-9]+([0-9]+)/i) {
-			$record->{'iss_ft_start'} = $1;
-		}
-		
-		if ($record->{'___Coverage Begins'} =~ /\(\s*(\d+)\s+(\w+)\s+(\d+)\s*\)/i) {
-			my ($day, $month, $year) = ($1, $2, $3);
-			$month = get_month($month, 'start');
-			$record->{'ft_start_date'} = sprintf("%04d-%02d-%02d", $year, $month, $day);
-		} elsif ($record->{'___Coverage Begins'} =~ /\(\s*([a-zA-Z]+)\s*-?\s*[a-zA-Z]*\s+(\d+)\s*\)/i) {
-			my ($month, $year) = ($1, $2);
-			$month = get_month($month, 'start');
-			$record->{'ft_start_date'} = sprintf("%04d-%02d", $year, $month);
-		} elsif ($record->{'___Coverage Begins'} =~ /\(\s*(\d+)/i) {
-			$record->{'ft_start_date'} = $1;
-		}
-	}
+    if ( defined( $record->{'___Coverage Begins'} ) ) {
+        if ( $record->{'___Coverage Begins'} =~ / vol [^0-9]+ ([0-9]+) /xsmi ) {
+            $record->{vol_ft_start} = $1;
+        }
+        if ( $record->{'___Coverage Begins'} =~ / iss [^0-9]+ ([0-9]+)/xsmi ) {
+            $record->{iss_ft_start} = $1;
+        }
 
-	if (defined($record->{'___Coverage Ends'})) {
-		if ($record->{'___Coverage Ends'} =~ /vol[^0-9]+[0-9]+-([0-9]+)/i) {
-			$record->{'vol_ft_end'} = $1;
-		} elsif ($record->{'___Coverage Ends'} =~ /vol[^0-9]+([0-9]+)/i) {
-			$record->{'vol_ft_end'} = $1;
-		}
-		if ($record->{'___Coverage Ends'} =~ /iss[^0-9]+[0-9]+-([0-9]+)/i) {
-			$record->{'iss_ft_end'} = $1;
-		} elsif ($record->{'___Coverage Ends'} =~ /iss[^0-9]+([0-9]+)/i) {
-			$record->{'iss_ft_end'} = $1;
-		}
-		
-		if ($record->{'___Coverage Ends'} =~ /\(\s*(\d+)\s+(\w+)\s+(\d+)\s*\)/i) {
-			my ($day, $month, $year) = ($1, $2, $3);
-			$month = get_month($month, 'end');
-			$record->{'ft_end_date'} = sprintf("%04d-%02d-%02d", $year, $month, $day);
-		} elsif ($record->{'___Coverage Ends'} =~ /\(\s*[a-zA-Z]*?\s*-?\s*([a-zA-Z]+)\s+(\d+)\s*\)/i) {
-			my ($month, $year) = ($1, $2);
-			$month = get_month($month, 'end');
-			$record->{'ft_end_date'} = sprintf("%04d-%02d", $year, $month);
-		} elsif ($record->{'___Coverage Ends'} =~ /(\d+)\s*\)/i) {
-			$record->{'ft_end_date'} = $1;
-		}
-	}		
+        if ( $record->{'___Coverage Begins'} =~ / \( \s* (\d+) \s+ (\w+) \s+ (\d{4}) \s* \) /xsmi )
+        {
+            my ( $day, $month, $year ) = ( $1, $2, $3 );
+            $month = get_month( $month, 'start' );
+            $record->{ft_start_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
+        }
+        elsif ( $record->{'___Coverage Begins'} =~ / \( \s* ([a-zA-Z]+) \s* -? \s* [a-zA-Z]* \s+ (\d{4}) \s* \) /xsmi )
+        {
+            my ( $month, $year ) = ( $1, $2 );
+            $month = get_month( $month, 'start' );
+            $record->{ft_start_date} = sprintf( "%04d-%02d", $year, $month );
+        }
+        elsif ( $record->{'___Coverage Begins'} =~ / \( \s* (\d{4}) /xsmi ) {
+            $record->{ft_start_date} = $1;
+        }
+    }
 
+    if ( defined( $record->{'___Coverage Ends'} ) ) {
+        if ( $record->{'___Coverage Ends'} =~ / vol [^0-9]+ [0-9]+ - ([0-9]+) /xsmi ) {
+            $record->{vol_ft_end} = $1;
+        }
+        elsif ( $record->{'___Coverage Ends'} =~ / vol [^0-9]+ ([0-9]+) /xsmi ) {
+            $record->{vol_ft_end} = $1;
+        }
+        if ( $record->{'___Coverage Ends'} =~ / iss [^0-9]+ [0-9]+ - ([0-9]+) /xsmi ) {
+            $record->{iss_ft_end} = $1;
+        }
+        elsif ( $record->{'___Coverage Ends'} =~ / iss [^0-9]+ ([0-9]+) /xsmi ) {
+            $record->{iss_ft_end} = $1;
+        }
 
-			
-	sub get_month {
-		my ($month, $period) = @_;
+        if ( $record->{'___Coverage Ends'} =~ / \( \s* (\d+) \s+ (\w+) \s+ (\d{4}) \s* \) /xsm )
+        {
+            my ( $day, $month, $year ) = ( $1, $2, $3 );
+            $month = get_month( $month, 'end' );
+            $record->{ft_end_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
+        }
+        elsif ( $record->{'___Coverage Ends'} =~ / \( \s* [a-zA-Z]*? \s* -? \s* ([a-zA-Z]+) \s+ (\d{4}) \s* \) /xsm )
+        {
+            my ( $month, $year ) = ( $1, $2 );
+            $month = get_month( $month, 'end' );
+            $record->{ft_end_date} = sprintf( "%04d-%02d", $year, $month );
+        }
+        elsif ( $record->{'___Coverage Ends'} =~ / (\d{4}) \s* \) /xsm ) {
+            $record->{ft_end_date} = $1;
+        }
+    }
 
-		if ($month =~ /^Jan/i) { return 1 }
-		elsif ($month =~ /^Feb/i) { return 2 }
-		elsif ($month =~ /^Mar/i) { return 3 }
-		elsif ($month =~ /^Apr/i) { return 4 }
-		elsif ($month =~ /^May/i) { return 5 }
-		elsif ($month =~ /^Jun/i) { return 6 }
-		elsif ($month =~ /^Jul/i) { return 7 }
-		elsif ($month =~ /^Aug/i) { return 8 }
-		elsif ($month =~ /^Sep/i) { return 9 }
-		elsif ($month =~ /^Oct/i) { return 10 }
-		elsif ($month =~ /^Nov/i) { return 11 }
-		elsif ($month =~ /^Dec/i) { return 12 }
-		elsif ($month =~ /^Spr/i) { return $period eq 'start' ? 1 : 6 }
-		elsif ($month =~ /^Sum/i) { return $period eq 'start' ? 3 : 9 }
-		elsif ($month =~ /^Fal/i) { return $period eq 'start' ? 6 : 12 }
-		elsif ($month =~ /^Aut/i) { return $period eq 'start' ? 6 : 12 }
-		elsif ($month =~ /^Win/i) { return $period eq 'start' ? 9 : 12 }
-		else { CUFTS::Exception::App->throw("Unable to find month match in fulltext date: $month") }
-	}	
+    sub get_month {
+        my ( $month, $period ) = @_;
 
-	return $class->SUPER::clean_data($record);
+        if    ( $month =~ /^Jan/i ) { return 1 }
+        elsif ( $month =~ /^Feb/i ) { return 2 }
+        elsif ( $month =~ /^Mar/i ) { return 3 }
+        elsif ( $month =~ /^Apr/i ) { return 4 }
+        elsif ( $month =~ /^May/i ) { return 5 }
+        elsif ( $month =~ /^Jun/i ) { return 6 }
+        elsif ( $month =~ /^Jul/i ) { return 7 }
+        elsif ( $month =~ /^Aug/i ) { return 8 }
+        elsif ( $month =~ /^Sep/i ) { return 9 }
+        elsif ( $month =~ /^Oct/i ) { return 10 }
+        elsif ( $month =~ /^Nov/i ) { return 11 }
+        elsif ( $month =~ /^Dec/i ) { return 12 }
+        elsif ( $month =~ /^Spr/i ) { return $period eq 'start' ? 1 : 6 }
+        elsif ( $month =~ /^Sum/i ) { return $period eq 'start' ? 3 : 9 }
+        elsif ( $month =~ /^Fal/i ) { return $period eq 'start' ? 6 : 12 }
+        elsif ( $month =~ /^Aut/i ) { return $period eq 'start' ? 6 : 12 }
+        elsif ( $month =~ /^Win/i ) { return $period eq 'start' ? 9 : 12 }
+        else {
+            CUFTS::Exception::App->throw("Unable to find month match in fulltext date: $month");
+        }
+    }
+
+    return $class->SUPER::clean_data($record);
 }
-
-
 
 sub build_linkJournal {
-	my ($class, $records, $resource, $site, $request) = @_;
-	
-	defined($records) && scalar(@$records) > 0 or 
-		return [];
-	defined($resource) or 
-		CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
-	defined($site) or 
-		CUFTS::Exception::App->throw('No site defined in build_linkJournal');
-	defined($request) or 
-		CUFTS::Exception::App->throw('No request defined in build_linkJournal');
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-	my @results;
-	foreach my $record (@$records) {
-		next unless assert_ne($record->issn);
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkJournal');
 
-		my $result = new CUFTS::Result;
-		$result->url('http://www.sciencedirect.com/science/journal/' . $record->issn);
-		$result->record($record);
+    my @results;
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->issn );
 
-		push @results, $result;
-	}
+        my $result = new CUFTS::Result;
+        $result->url('http://www.sciencedirect.com/science/journal/' . $record->issn );
+        $result->record($record);
 
-	return \@results;
+        push @results, $result;
+    }
+
+    return \@results;
 }
-
-
 
 1;
