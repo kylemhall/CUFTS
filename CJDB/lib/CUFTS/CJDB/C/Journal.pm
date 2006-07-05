fd98ba2aee307e1730955cbbc6ada71da87fd4d9
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-07-05 18:45:25 +0000

    Added support for RSS feeds

--- CJDB/lib/CUFTS/CJDB/C/Journal.pm
+++ CJDB/lib/CUFTS/CJDB/C/Journal.pm
@@ -2,7 +2,9 @@ package CUFTS::CJDB::C::Journal;
 
 use strict;
 use base 'Catalyst::Base';
-
+use CUFTS::Util::Simple;
+use XML::RAI;
+use LWP::Simple;
 
 sub view : Private {
 	my ($self, $c, $journals_auth_id) = @_;
@@ -24,6 +26,31 @@ sub view : Private {
 		);
 		$c->stash->{my_tags} = \@my_tags;
 	}
+
+    if ( not_empty_string($journal->rss) ) {
+        my $xml = get($journal->rss);
+        
+        XML::RSS::Parser->register_ns_prefix( 'prism', 'http://purl.org/rss/1.0/modules/prism/' );
+        
+        XML::RAI::Channel->add_mapping( 'year',   'year'   );
+        XML::RAI::Channel->add_mapping( 'month',  'month'  );
+        XML::RAI::Channel->add_mapping( 'day',    'day'    );
+        XML::RAI::Channel->add_mapping( 'date',   'prism:coverDisplayDate' );
+        
+        XML::RAI::Item->add_mapping( 'volume',     'volume',    'prism:volume' );
+        XML::RAI::Item->add_mapping( 'issue',      'issue',     'prism:number' );
+        XML::RAI::Item->add_mapping( 'startPage',  'startPage', 'prism:startingPage' );
+        XML::RAI::Item->add_mapping( 'endPage',    'endPage',   'prism:endingPage' );
+        XML::RAI::Item->add_mapping( 'date',       'pubDate',   'prism:publicationDate' );
+        XML::RAI::Item->add_mapping( 'authors',    'authors',   'author', 'dc:creator' );
+        XML::RAI::Item->add_mapping( 'publisher',   'dc:publisher' );
+        
+        
+        eval {
+            $c->stash->{rss} = XML::RAI->parse_string($xml);
+        };
+        
+    }
 
 	$c->stash->{tags} = CJDB::DB::Tags->get_tag_summary($journals_auth_id, $c->stash->{current_site}->id, (defined($c->stash->{current_account}) ? $c->stash->{current_account}->id : undef));
 	$c->stash->{journal} = $journal;	
