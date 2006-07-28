c87063b1714af45037f6d9ac97f81b7d0e578f27
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-07-28 20:41:33 +0000

    Added loader for modules which adds them all to a list so that dropdowns can be presented in the MaintTool

--- trunk/CUFTS/lib/CUFTS/ResourcesLoader.pm
+++ trunk/CUFTS/lib/CUFTS/ResourcesLoader.pm
@@ -4,80 +4,100 @@
 ##
 ## This file is part of CUFTS.
 ##
-## CUFTS is free software; you can redistribute it and/or modify it under
+## CUFTS is free software you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free
-## Software Foundation; either version 2 of the License, or (at your option)
+## Software Foundation either version 2 of the License, or (at your option)
 ## any later version.
 ##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
-## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 ## details.
 ##
 ## You should have received a copy of the GNU General Public License along
-## with CUFTS; if not, write to the Free Software Foundation, Inc., 59
+## with CUFTS if not, write to the Free Software Foundation, Inc., 59
 ## Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package CUFTS::ResourcesLoader;
 
 use strict;
 
-use CUFTS::Resources::ATLA;
-use CUFTS::Resources::BioLine;
-use CUFTS::Resources::BioMed;
-use CUFTS::Resources::BioOne;
-use CUFTS::Resources::Blackwell;
-use CUFTS::Resources::blank;
-use CUFTS::Resources::Cambridge;
-use CUFTS::Resources::CH_PCI;
-use CUFTS::Resources::Chicago;
-use CUFTS::Resources::CrossRef;
-use CUFTS::Resources::Dekker;
-use CUFTS::Resources::DOAJ;
-use CUFTS::Resources::EBSCO_CINAHL;
-use CUFTS::Resources::EBSCO_EJS;
-use CUFTS::Resources::EBSCO_Generic;
-use CUFTS::Resources::EBSCO_II;
-use CUFTS::Resources::EBSCO;
-use CUFTS::Resources::Elsevier_CNSLP;
-use CUFTS::Resources::Elsevier;
-#use CUFTS::Resources::ElsevierEurope;
-use CUFTS::Resources::Emerald;
-use CUFTS::Resources::Erudit;
-use CUFTS::Resources::Extenza;
-use CUFTS::Resources::Gale_CWI;
-use CUFTS::Resources::Gale;
-use CUFTS::Resources::GenericJournal;
-use CUFTS::Resources::GenericJournalDOI;
-use CUFTS::Resources::Highwire;
-use CUFTS::Resources::History_Coop;
-use CUFTS::Resources::IEEE;
-use CUFTS::Resources::III;
-use CUFTS::Resources::Ingenta;
-use CUFTS::Resources::IngentaConnect;
-use CUFTS::Resources::JSTOR;
-use CUFTS::Resources::Karger;
-use CUFTS::Resources::Kluwer;
-use CUFTS::Resources::Lexis_Nexis_AC;
-use CUFTS::Resources::MetaPress;
-use CUFTS::Resources::Micromedia_CNS;
-use CUFTS::Resources::Micromedia;
-use CUFTS::Resources::Muse;
-use CUFTS::Resources::NRC;
-use CUFTS::Resources::OCLC;
-use CUFTS::Resources::Ovid;
-use CUFTS::Resources::Oxford;
-#use CUFTS::Resources::PCI;
-use CUFTS::Resources::Proquest_CNS;
-use CUFTS::Resources::Proquest;
-use CUFTS::Resources::ProquestMicromedia;
-use CUFTS::Resources::PubMed;
-use CUFTS::Resources::PubMedCentral;
-use CUFTS::Resources::SageCSA;
-use CUFTS::Resources::SIRSI;
-use CUFTS::Resources::Springer;
-use CUFTS::Resources::Swets;
-use CUFTS::Resources::Wiley;
-use CUFTS::Resources::Wilson;
+my @resource_modules;
+
+BEGIN {
+
+    @resource_modules = qw(
+        ATLA
+        BioLine
+        BioMed
+        BioOne
+        Blackwell
+        blank
+        Cambridge
+        CH_PCI
+        Chicago
+        CrossRef
+        Dekker
+        DOAJ
+        EBSCO_CINAHL
+        EBSCO_EJS
+        EBSCO_Generic
+        EBSCO_II
+        EBSCO
+        Elsevier_CNSLP
+        Elsevier
+        Emerald
+        Erudit
+        Extenza
+        Gale_CWI
+        Gale
+        GenericJournal
+        GenericJournalDOI
+        Highwire
+        History_Coop
+        IEEE
+        III
+        Ingenta
+        IngentaConnect
+        JSTOR
+        Karger
+        Kluwer
+        Lexis_Nexis_AC
+        MetaPress
+        Micromedia_CNS
+        Micromedia
+        Muse
+        NRC
+        OCLC
+        Ovid
+        Oxford
+        Proquest_CNS
+        Proquest
+        ProquestMicromedia
+        PubMed
+        PubMedCentral
+        SageCSA
+        SIRSI
+        Springer
+        Swets
+        Wiley
+        Wilson
+    );
+
+    # ElsevierEurope
+    # PCI
+
+    foreach my $module ( @resource_modules ) {
+        my $load_module = "CUFTS::Resources::${module}.pm";
+        $load_module =~ s{::}{/}xsmg;
+        require $load_module;
+    }
+}
+
+sub list_modules {
+    my ( $class ) = @_;
+    return @resource_modules;
+}
+
 
 1;
