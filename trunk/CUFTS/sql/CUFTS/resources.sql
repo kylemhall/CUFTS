05fc8b92d727c9a7180b9d8995dac9e317859be3
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-08-21 22:30:00 +0000

    Added key field

--- trunk/CUFTS/sql/CUFTS/resources.sql
+++ trunk/CUFTS/sql/CUFTS/resources.sql
@@ -1,20 +1,18 @@
 CREATE TABLE resources (
-	id		    SERIAL PRIMARY KEY,
+    id            SERIAL PRIMARY KEY,
 
-	key         VARCHAR(256),
-	
-	name		VARCHAR(256) NOT NULL,
+    key              VARCHAR(256),
+    name             VARCHAR(256) NOT NULL,
 
-	resource_type	INTEGER NOT NULL,	/* id for resource type */
+    resource_type    INTEGER NOT NULL,    /* id for resource type */
 
-	provider	VARCHAR(256),
-	module		VARCHAR(256) NOT NULL,
+    provider         VARCHAR(256),
+    module           VARCHAR(256) NOT NULL,
 
-	active		BOOLEAN NOT NULL DEFAULT TRUE,
-	
-	title_list_scanned	TIMESTAMP,
-
-	title_count	INTEGER,
+    active           BOOLEAN NOT NULL DEFAULT TRUE,
+    
+    title_list_scanned    TIMESTAMP,
+    title_count           INTEGER,
 
     resource_identifier VARCHAR(256),
     database_url        VARCHAR(1024),
@@ -23,6 +21,8 @@ CREATE TABLE resources (
     url_base            VARCHAR(1024),
     proxy_suffix        VARCHAR(1024),
 
-	created		TIMESTAMP NOT NULL DEFAULT NOW(),
-	modified	TIMESTAMP NOT NULL DEFAULT NOW()
+    created     TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified    TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE UNIQUE INDEX resources_key_idx ON resources (key);
