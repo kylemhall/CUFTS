12fee4095c1c7b7f686fe2d342b3c3813d3dd2d1
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-10-20 22:52:14 +0000

    Updated to use new generic SFX loader

--- lib/CUFTS/Resources/Blackwell.pm
+++ lib/CUFTS/Resources/Blackwell.pm
@@ -20,7 +20,7 @@
 
 package CUFTS::Resources::Blackwell;
 
-use base qw(CUFTS::Resources::GenericJournalDOI);
+use base qw(CUFTS::Resources::Base::SFXLoader CUFTS::Resources::Base::DOI);
 
 use CUFTS::Exceptions;
 use CUFTS::Util::Simple;
@@ -50,19 +50,6 @@ sub title_list_fields {
             journal_url
         )
     ];
-}
-
-## title_list_field_map - Hash ref mapping fields from the raw title lists to
-## internal field names
-##
-
-sub title_list_field_map {
-    return {
-        'Print ISSN'   => 'issn',
-        'Online ISSN'  => 'e_issn',
-        'Publication'  => 'title',
-        'URL'          => 'journal_url',
-    };
 }
 
 ## global_resource_details - Controls which details are displayed on the global
@@ -117,106 +104,31 @@ sub overridable_resource_details {
     ];
 }
 
-sub title_list_skip_lines_count { return 1; }
+sub build_linkJournal {
+    my ( $class, $records, $resource, $site, $request ) = @_;
 
-sub clean_data {
-    my ( $class, $record ) = @_;
-    
-    if ( $record->{'___First Online Issue'} =~ / Vol\. \s+ ([^,]+), \s+ No\. \s+ ([^,]+), (.+) /xsm ) {
-        my ( $vol, $no, $date ) = ( $1, $2, $3 );
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkJournal');
 
-        # Parse out vol varients: 1   1-2   1&2   s1
-        $record->{vol_ft_start}  = parse_vol_iss($vol, 0);
-        $record->{iss_ft_start}  = parse_vol_iss($no,  0);
-        $record->{ft_start_date} = parse_date($date, 0); 
+    my @results;
 
-    }
-    elsif ( $record->{'___First Online Issue'} ne 'Online early issue' ) {
-        warn("Unable to parse holding line: " . $record->{'___First Online Issue'} );
-    }
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->journal_url );
 
-    if ( $record->{'___Last Online Issue'} =~ / Vol\. \s+ ([^,]+), \s+ No\. \s+ ([^,]+), (.+) /xsm ) {
-        my ( $vol, $no, $date ) = ( $1, $2, $3 );
+        my $result = new CUFTS::Result( $record->journal_url );
+        $result->record($record);
 
-        # Parse out vol varients: 1   1-2   1&2   s1
-        $record->{vol_ft_end}  = parse_vol_iss($vol, 1);
-        $record->{iss_ft_end}  = parse_vol_iss($no,  1);
-        $record->{ft_end_date} = parse_date($date, 1);
-        if ( !defined($record->{ft_end_date}) ) {
-            delete $record->{vol_ft_end};
-            delete $record->{iss_ft_end};
-        }
-
-    }
-    elsif ( $record->{'___Last Online Issue'} ne 'Online early issue' ) {
-        warn("Unable to parse holding line: " . $record->{'___Last Online Issue'} );
+        push @results, $result;
     }
 
-    return $record;
+    return \@results;
 }
 
-
-sub parse_vol_iss {
-    my ( $string, $end ) = @_;
-
-    # Parse out varients: 1   1-2   1&2   s1
-    
-    $string =~ tr/a-zA-Z//;
-    my @vals = split /[^\d]/, $string;
-    
-    return $end ? $vals[$#vals] : $vals[0];
-}
-    
-
-sub parse_date {
-    my ( $string, $end ) = @_;
-    
-    if ( $string =~ /^ \s* (.*) \s+ (\d{4}) /xsm ) {
-        my ( $month, $year ) = ( $1, $2 );
-        
-        my @months = split /[^a-z]+/, lc($month);
-        $month = $end ? $months[$#months] : $months[0];
-        my $numeric_month;
-
-        if ( !defined($month) ) {
-            $month = ''
-        };
-
-        if    ( $month =~ /^\s*jan/i )     { $numeric_month =  '01' }
-        elsif ( $month =~ /^\s*feb/i )     { $numeric_month =  '02' }
-        elsif ( $month =~ /^\s*mar/i )     { $numeric_month =  '03' }
-        elsif ( $month =~ /^\s*a[pv]r/i )  { $numeric_month =  '04' }
-        elsif ( $month =~ /^\s*may/i )     { $numeric_month =  '05' }
-        elsif ( $month =~ /^\s*jun/i )     { $numeric_month =  '06' }
-        elsif ( $month =~ /^\s*jul/i )     { $numeric_month =  '07' }
-        elsif ( $month =~ /^\s*aug/i )     { $numeric_month =  '08' }
-        elsif ( $month =~ /^\s*sep/i )     { $numeric_month =  '09' }
-        elsif ( $month =~ /^\s*oct/i )     { $numeric_month =  '10' }
-        elsif ( $month =~ /^\s*nov/i )     { $numeric_month =  '11' }
-        elsif ( $month =~ /^\s*dec/i )     { $numeric_month =  '12' }
-        else {
-            $numeric_month = $end ? 12 : 1;
-        }
-        my $day = $end ? ( 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 )[$numeric_month - 1] : '01';
-
-        my $date = "$year-$numeric_month-$day";
-        if ( !$end ) {
-            return $date;
-
-        }
-        
-        if ( Delta_Days( $year, $numeric_month, $day, Today() ) > 180 ) {
-            return $date
-        }
-        else {
-            return undef;
-        }
-    }
-    else {
-        warn("Unable to parse date from: $string");
-        return undef;
-    }
-    
-}
 
 1;
