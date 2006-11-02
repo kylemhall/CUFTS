e6f46262c5edc9048063b34716295fbdd7c16eec
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2006-11-02 19:09:10 +0000

    Go directly to DOIs at Blackwells due to errors in Crossref redirecting.  Faster, too.

--- lib/CUFTS/Resources/Blackwell.pm
+++ lib/CUFTS/Resources/Blackwell.pm
@@ -20,7 +20,7 @@
 
 package CUFTS::Resources::Blackwell;
 
-use base qw(CUFTS::Resources::Base::SFXLoader CUFTS::Resources::Base::DOI);
+use base qw(CUFTS::Resources::Base::SFXLoader);
 
 use CUFTS::Exceptions;
 use CUFTS::Util::Simple;
@@ -128,6 +128,39 @@ sub build_linkJournal {
     }
 
     return \@results;
+}
+
+sub build_linkFulltext {
+	my ($class, $records, $resource, $site, $request) = @_;
+
+	defined($records) && scalar(@$records) > 0 or 
+		return [];
+	defined($resource) or 
+		CUFTS::Exception::App->throw('No resource defined in build_linkFulltext');
+	defined($site) or 
+		CUFTS::Exception::App->throw('No site defined in build_linkFulltext');
+	defined($request) or 
+		CUFTS::Exception::App->throw('No request defined in build_linkFulltext');
+
+	if ( not_empty_string($request->doi) ) {
+		my $url;
+		$url .= 'http://www.blackwell-synergy.com/doi/abs/';
+		$url .= uri_escape($request->doi, "^A-Za-z0-9\-_.!~*'()\/");
+
+		my $result = new CUFTS::Result($url);
+		$result->record($records->[0]);
+		
+		return [$result];
+	} else {
+		return [];
+	}
+}
+
+sub can_getFulltext {
+	my ($class, $request) = @_;
+
+	return 1 if not_empty_string($request->doi);
+	return 0;	
 }
 
 
