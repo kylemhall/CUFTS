9ebc7b2f42d8c88bbab76dbd6bb0d1183de8d34d
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-02-12 22:44:04 +0000

    Fixed typo

--- trunk/CUFTS/sql/CUFTS/views/journals_active.sql
+++ trunk/CUFTS/sql/CUFTS/views/journals_active.sql
@@ -31,6 +31,6 @@ CREATE VIEW journals_active AS
            journals.abbreviation,
            journals.current_months,
            journals.current_years,
-           jouranls.cjdb_note 
+           journals.cjdb_note 
     FROM (journals JOIN local_journals ON ((local_journals.journal = journals.id))) 
     WHERE (local_journals.active = true);
