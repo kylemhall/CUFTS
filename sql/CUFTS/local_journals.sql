23f7c8a573cadf91cda274fdb6a3890a7aac9e1d
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-02-13 19:36:28 +0000

    Changed SQL to match new no-details layout.  NOT TESTED FOR NEW INSTALLS YET

--- sql/CUFTS/local_journals.sql
+++ sql/CUFTS/local_journals.sql
@@ -1,37 +1,48 @@
 CREATE TABLE local_journals (
-	id		SERIAL PRIMARY KEY,
-	title		VARCHAR(512),
-	issn		VARCHAR(8),		/* ISSN with no dash */
-	e_issn		VARCHAR(8),		/* Electronic ISSN if one exists (no dash) */
+    id              SERIAL PRIMARY KEY,
+    title           VARCHAR(512),
+    issn            VARCHAR(8),        /* ISSN with no dash */
+    e_issn          VARCHAR(8),        /* Electronic ISSN if one exists (no dash) */
 
-	resource	INTEGER NOT NULL,	/* id for local resource journal is in */
-	journal		INTEGER,		/* id for a record in the journal table if relevant */
+    resource        INTEGER NOT NULL,    /* id for local resource journal is in */
+    journal         INTEGER,        /* id for a record in the journal table if relevant */
 
-	active		BOOLEAN NOT NULL DEFAULT true,
-	
-	vol_cit_start	VARCHAR(128),		/* Starting volume for citations */
-	vol_cit_end	VARCHAR(128),		/* Ending volume for citations */
-	vol_ft_start	VARCHAR(128),		/* Starting volume for fulltext */
-	vol_ft_end	VARCHAR(128),		/* Ending volume for fulltext */
+    active          BOOLEAN NOT NULL DEFAULT true,
+    
+    vol_cit_start   VARCHAR(128),        /* Starting volume for citations */
+    vol_cit_end     VARCHAR(128),        /* Ending volume for citations */
+    vol_ft_start    VARCHAR(128),        /* Starting volume for fulltext */
+    vol_ft_end      VARCHAR(128),        /* Ending volume for fulltext */
 
-	iss_cit_start	VARCHAR(128),		/* Starting issue for citations */
-	iss_cit_end	VARCHAR(128),		/* Ending issue for citations */
-	iss_ft_start	VARCHAR(128),		/* Starting issue for fulltext */
-	iss_ft_end	VARCHAR(128),		/* Ending issue for fulltext */
+    iss_cit_start   VARCHAR(128),        /* Starting issue for citations */
+    iss_cit_end     VARCHAR(128),        /* Ending issue for citations */
+    iss_ft_start    VARCHAR(128),        /* Starting issue for fulltext */
+    iss_ft_end      VARCHAR(128),        /* Ending issue for fulltext */
 
-	cit_start_date	DATE,			/* Starting date for citations */
-	cit_end_date	DATE,			/* Ending date for citations */
-	ft_start_date	DATE,			/* Starting date for fulltext */
-	ft_end_date	DATE,			/* Ending date for fulltext */
+    cit_start_date  DATE,            /* Starting date for citations */
+    cit_end_date    DATE,            /* Ending date for citations */
+    ft_start_date   DATE,            /* Starting date for fulltext */
+    ft_end_date     DATE,            /* Ending date for fulltext */
 
-	embargo_months	INTEGER,		/* Number of months title is embargoed */
-	embargo_days	INTEGER,		/* Number of days title is embargoed */
+    embargo_months  INTEGER,        /* Number of months title is embargoed */
+    embargo_days    INTEGER,        /* Number of days title is embargoed */
 
     journal_auth    INTEGER,
+    
+    db_identifier   VARCHAR(256),   /* code used to identify the journal within the vendor's database */
+    toc_url         VARCHAR(1024),  /* URL for the table of contents */
+    journal_url     VARCHAR(1024),  /* URL for the journal */
+    urlbase         VARCHAR(1024),  /* URL base used for creating links */
+    publisher       VARCHAR(1024),  /* Journal publisher */
+    abbreviation    VARCHAR(1024),  /* Journal abbreviation */
 
-	created		TIMESTAMP NOT NULL DEFAULT NOW(),
-	scanned		TIMESTAMP NOT NULL DEFAULT NOW(),
-	modified	TIMESTAMP NOT NULL DEFAULT NOW()
+    current_months  VARCHAR(256),   /* Months of moving wall access to current issues */
+    current_years   VARCHAR(256),   /* Years of moving wall access to current issues */
+    cjdb_note       TEXT,           /* Note to display in CJDB (global may not be used, local only) */
+
+    created        TIMESTAMP NOT NULL DEFAULT NOW(),
+    scanned        TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified       TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX local_journals_issn_idx ON local_journals (issn) WHERE issn IS NOT NULL;
