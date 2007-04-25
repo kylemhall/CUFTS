72070bc908a75694310ae817fa403110f077052a
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-04-25 22:04:41 +0000

    Added support for backslash delimiter for commas

--- lib/CUFTS/Util/CSVParse.pm
+++ lib/CUFTS/Util/CSVParse.pm
@@ -15,24 +15,24 @@ use strict;
 #    newly created Text::CSV object.
 ################################################################################
 sub new {
-  my $proto = shift;
-  my $class = ref($proto) || $proto;
-  my $self = {};
-  $self->{'_STATUS'} = undef;
-  $self->{'_ERROR_INPUT'} = undef;
-  $self->{'_STRING'} = undef;
-  $self->{'_FIELDS'} = undef;
-  $self->{'_DELIM'} = ',';
-  bless $self, $class;
-  return $self;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = {};
+    $self->{'_STATUS'}      = undef;
+    $self->{'_ERROR_INPUT'} = undef;
+    $self->{'_STRING'}      = undef;
+    $self->{'_FIELDS'}      = undef;
+    $self->{'_DELIM'}       = ',';
+    bless $self, $class;
+    return $self;
 }
 
 sub delim {
-	my $self = shift;
-	my $set = shift;
-	defined($set) and 
-		$self->{'_DELIM'} = $set;
-	return $self->{'_DELIM'};
+    my $self = shift;
+    my $set  = shift;
+    defined($set)
+        and $self->{'_DELIM'} = $set;
+    return $self->{'_DELIM'};
 }
 
 ################################################################################
@@ -42,8 +42,8 @@ sub delim {
 #    or parse().  there are no side-effects.
 ################################################################################
 sub status {
-  my $self = shift;
-  return $self->{'_STATUS'};
+    my $self = shift;
+    return $self->{'_STATUS'};
 }
 
 ################################################################################
@@ -53,8 +53,8 @@ sub status {
 #    combine() or parse().  there are no side-effects.
 ################################################################################
 sub error_input {
-  my $self = shift;
-  return $self->{'_ERROR_INPUT'};
+    my $self = shift;
+    return $self->{'_ERROR_INPUT'};
 }
 
 ################################################################################
@@ -65,8 +65,8 @@ sub error_input {
 #    side-effects.
 ################################################################################
 sub string {
-  my $self = shift;
-  return $self->{'_STRING'};
+    my $self = shift;
+    return $self->{'_STRING'};
 }
 
 ################################################################################
@@ -77,13 +77,12 @@ sub string {
 #    side-effects.
 ################################################################################
 sub fields {
-  my $self = shift;
-  if (ref($self->{'_FIELDS'})) {
-    return @{$self->{'_FIELDS'}};
-  }
-  return undef;
+    my $self = shift;
+    if ( ref( $self->{'_FIELDS'} ) ) {
+        return @{ $self->{'_FIELDS'} };
+    }
+    return undef;
 }
-
 
 ################################################################################
 # parse
@@ -98,33 +97,36 @@ sub fields {
 #      setting error_input()
 ################################################################################
 sub parse {
-  my $self = shift;
-  $self->{'_STRING'} = shift;
-  $self->{'_FIELDS'} = undef;
-  $self->{'_ERROR_INPUT'} = $self->{'_STRING'};
-  $self->{'_STATUS'} = 0;
-  if (!defined($self->{'_STRING'})) {
-    return $self->{'_STATUS'};
-  }
-  my $keep_biting = 1;
-  my $palatable = 0;
-  my $line = $self->{'_STRING'};
-  if ($line =~ /\n$/) {
-    chop($line);
-    if ($line =~ /\r$/) {
-      chop($line);
+    my $self = shift;
+    $self->{'_STRING'}      = shift;
+    $self->{'_FIELDS'}      = undef;
+    $self->{'_ERROR_INPUT'} = $self->{'_STRING'};
+    $self->{'_STATUS'}      = 0;
+    if ( !defined( $self->{'_STRING'} ) ) {
+        return $self->{'_STATUS'};
     }
-  }
-  my $mouthful = '';
-  my @part = ();
-  while ($keep_biting and ($palatable = $self->_bite(\$line, \$mouthful, \$keep_biting))) {
-    push(@part, $mouthful);
-  }
-  if ($palatable) {
-    $self->{'_ERROR_INPUT'} = undef;
-    $self->{'_FIELDS'} = \@part;
-  }
-  return $self->{'_STATUS'} = $palatable;
+    my $keep_biting = 1;
+    my $palatable   = 0;
+    my $line        = $self->{'_STRING'};
+    if ( $line =~ /\n$/ ) {
+        chop($line);
+        if ( $line =~ /\r$/ ) {
+            chop($line);
+        }
+    }
+    my $mouthful = '';
+    my @part     = ();
+    while ( $keep_biting
+        and ( $palatable = $self->_bite( \$line, \$mouthful, \$keep_biting ) )
+        )
+    {
+        push( @part, $mouthful );
+    }
+    if ($palatable) {
+        $self->{'_ERROR_INPUT'} = undef;
+        $self->{'_FIELDS'}      = \@part;
+    }
+    return $self->{'_STATUS'} = $palatable;
 }
 
 ################################################################################
@@ -141,8 +143,8 @@ sub parse {
 #    returning the remaining string, the "piece" bitten, and if there's any
 #    more.  failure can be the result of the csv string containing an invalid
 #    sequence of characters.
-#   
-#    from the csv string and 
+#
+#    from the csv string and
 #    to be a valid comma-separated value.  failure can be the result of
 #    no arguments or an argument containing an invalid sequence of characters.
 #    side-effects include:
@@ -152,96 +154,113 @@ sub parse {
 #      setting error_input()
 ################################################################################
 sub _bite {
-  my ($self, $line_ref, $piece_ref, $bite_again_ref) = @_;
-  my $in_quotes = 0;
-  my $ok = 0;
-  $$piece_ref = '';
-  $$bite_again_ref = 0;
-  while (1) {
-    if (length($$line_ref) < 1) {
+    my ( $self, $line_ref, $piece_ref, $bite_again_ref ) = @_;
+    my $in_quotes = 0;
+    my $ok        = 0;
+    $$piece_ref      = '';
+    $$bite_again_ref = 0;
+    while (1) {
+        if ( length($$line_ref) < 1 ) {
 
-      # end of string...
-      if ($in_quotes) {
+            # end of string...
+            if ($in_quotes) {
 
-	# end of string, missing closing double-quote...
-	last;
-      } else {
+                # end of string, missing closing double-quote...
+                last;
+            }
+            else {
 
-	# proper end of string...
-	$ok = 1;
-	last;
-      }
-    } elsif ($$line_ref =~ /^\042/) {
+                # proper end of string...
+                $ok = 1;
+                last;
+            }
+        }
+        elsif ( $$line_ref =~ /^\042/ ) {
 
-      # double-quote...
-      if ($in_quotes) {
-	if (length($$line_ref) == 1) {
+            # double-quote...
+            if ($in_quotes) {
+                if ( length($$line_ref) == 1 ) {
 
-	  # closing double-quote at end of string...
-	  substr($$line_ref, 0, 1) = '';
-	  $ok = 1;
-	  last;
-	} elsif ($$line_ref =~ /^\042\042/) {
+                    # closing double-quote at end of string...
+                    substr( $$line_ref, 0, 1 ) = '';
+                    $ok = 1;
+                    last;
+                }
+                elsif ( $$line_ref =~ /^\042\042/ ) {
 
-	  # an embedded double-quote...
-	  $$piece_ref .= "\042";
-	  substr($$line_ref, 0, 2) = '';
-	} elsif ($$line_ref =~ /^\042$self->{'_DELIM'}/o) {
+                    # an embedded double-quote...
+                    $$piece_ref .= "\042";
+                    substr( $$line_ref, 0, 2 ) = '';
+                }
+                elsif ( $$line_ref =~ /^\042$self->{'_DELIM'}/o ) {
 
-	  # closing double-quote followed by a comma...
-	  substr($$line_ref, 0, 2) = '';
-	  $$bite_again_ref = 1;
-	  $ok = 1;
-	  last;
-	} else {
+                    # closing double-quote followed by a comma...
+                    substr( $$line_ref, 0, 2 ) = '';
+                    $$bite_again_ref = 1;
+                    $ok              = 1;
+                    last;
+                }
+                else {
 
-	  # double-quote, followed by undesirable character (bad character sequence)...
-	  last;
-	}
-      } else {
-	if (length($$piece_ref) < 1) {
+ # double-quote, followed by undesirable character (bad character sequence)...
+                    last;
+                }
+            }
+            else {
+                if ( length($$piece_ref) < 1 ) {
 
-	  # starting double-quote at beginning of string
-	  $in_quotes = 1;
-	  substr($$line_ref, 0, 1) = '';
-	} else {
+                    # starting double-quote at beginning of string
+                    $in_quotes = 1;
+                    substr( $$line_ref, 0, 1 ) = '';
+                }
+                else {
 
-	  # double-quote, outside of double-quotes (bad character sequence)...
-	  last;
-	}
-      }
-    } elsif ($$line_ref =~ /^$self->{'_DELIM'}/o) {
+          # double-quote, outside of double-quotes (bad character sequence)...
+                    last;
+                }
+            }
+        }
+        elsif ( $$line_ref =~ /^\\$self->{'_DELIM'}/o ) {
+        
+            # backslash followed by a comma 
+            $$piece_ref .= substr( $$line_ref, 1, 1 );
+            substr( $$line_ref, 0, 2 ) = '';
 
-      # comma...
-      if ($in_quotes) {
+        }
+        elsif ( $$line_ref =~ /^$self->{'_DELIM'}/o ) {
 
-	# a comma, inside double-quotes...
-	$$piece_ref .= substr($$line_ref, 0 ,1);
-	substr($$line_ref, 0, 1) = '';
-      } else {
+            # comma...
+            if ($in_quotes) {
 
-	# a comma, which separates values...
-	substr($$line_ref, 0, 1) = '';
-	$$bite_again_ref = 1;
-	$ok = 1;
-	last;
-      }
-    } elsif ($$line_ref =~ /^[\t\040-\176]/) {
+                # a comma, inside double-quotes...
+                $$piece_ref .= substr( $$line_ref, 0, 1 );
+                substr( $$line_ref, 0, 1 ) = '';
+            }
+            else {
 
-      # a tab, space, or printable...
-      $$piece_ref .= substr($$line_ref, 0 ,1);
-      substr($$line_ref, 0, 1) = '';
-    } else {
+                # a comma, which separates values...
+                substr( $$line_ref, 0, 1 ) = '';
+                $$bite_again_ref = 1;
+                $ok              = 1;
+                last;
+            }
+        }
+        elsif ( $$line_ref =~ /^[\t\040-\176]/ ) {
 
-      $$piece_ref .= substr($$line_ref, 0 ,1);
-      substr($$line_ref, 0, 1) = '';
+            # a tab, space, or printable...
+            $$piece_ref .= substr( $$line_ref, 0, 1 );
+            substr( $$line_ref, 0, 1 ) = '';
+        }
+        else {
+
+            $$piece_ref .= substr( $$line_ref, 0, 1 );
+            substr( $$line_ref, 0, 1 ) = '';
+        }
     }
-  }
-  return $ok;
+    return $ok;
 }
 
-
-1; 
+1;
 
 __END__
 
