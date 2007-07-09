cb8663fa41f21b0b3d2be128355bddb1988cbd2a
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-07-09 18:58:57 +0000

    Updated jQuery New fast browse system

--- CJDB/lib/CUFTS/CJDB/C/Resources.pm
+++ CJDB/lib/CUFTS/CJDB/C/Resources.pm
@@ -5,15 +5,52 @@ use base 'Catalyst::Base';
 use CUFTS::Util::Simple;
 
 use Data::Dumper;
-
+use JSON::XS;
 use CUFTS::DB::LocalResources;
 
 sub default : Private {
     my ( $self, $c ) = @_;
 
-    warn('default');
-
     $c->redirect('/resources/browse/');
+}
+
+sub resource : Local {
+    my ( $self, $c, $id ) = @_;
+
+    $c->stash->{erm} = CUFTS::DB::ERMMain->retrieve($id);
+    $c->stash->{template} = 'resources/resource.tt';
+}
+
+sub ajax_resource : Local {
+    my ( $self, $c, $id ) = @_;
+
+    
+    my $erm_obj = CUFTS::DB::ERMMain->retrieve( $id );
+    my $erm_hash = {
+        subjects => [],
+        content_types => [],
+    };
+    
+    foreach my $column ( $erm_obj->columns() ) {
+        $erm_hash->{$column} = $erm_obj->$column();
+    }
+    foreach my $column ( qw( consortia cost_base resource_medium resource_type ) ) {
+        if ( defined( $erm_hash->{$column} ) ) {
+            $erm_hash->{$column} = $erm_obj->$column()->$column();
+        }
+    }
+
+    my @subjects = $erm_obj->subjects;
+    foreach my $subject ( @subjects ) {
+        push @{ $erm_hash->{subjects} }, $subject->subject;
+    }
+
+    my @content_types = $erm_obj->content_types;
+    foreach my $content_type ( @content_types ) {
+        push @{ $erm_hash->{content_types} }, $content_type->content_type;
+    }
+
+    $c->res->body( to_json( $erm_hash ) );
 }
 
 sub list : Local {
