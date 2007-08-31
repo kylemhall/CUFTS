7ed45c4764e7e0ee30dba58aae5b0e4c0fc696bd
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-08-31 17:20:38 +0000

    Fixed journals_titles.sql containing associations table

--- trunk/CUFTS/sql/CJDB/journals_titles.sql
+++ trunk/CUFTS/sql/CJDB/journals_titles.sql
@@ -1,9 +1,10 @@
-CREATE TABLE cjdb_journals_associations (
+CREATE TABLE cjdb_journals_titles (
     id              SERIAL PRIMARY KEY,
     journal         INTEGER NOT NULL,
-    association     INTEGER NOT NULL,
-    site            INTEGER NOT NULL
+    title           INTEGER NOT NULL,
+    site            INTEGER NOT NULL,
+    main            INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE INDEX cjdb_journals_associations_j_idx ON cjdb_associations (journal);
-CREATE INDEX cjdb_journals_associations_sa_idx ON cjdb_associations (site, association);
+CREATE INDEX journ_ttl_j_idx ON cjdb_journals_titles (journal);
+CREATE INDEX journ_ttl_s_t_idx ON cjdb_journals_titles (site, title);
