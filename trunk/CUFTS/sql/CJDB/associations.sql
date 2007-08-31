0ecc9ae1fd09ae7b02c4274de68601567e26bc2f
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-08-31 17:31:13 +0000

    Fixed typo in associations.sql

--- trunk/CUFTS/sql/CJDB/associations.sql
+++ trunk/CUFTS/sql/CJDB/associations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cjdb_associations (
     id                  SERIAL PRIMARY KEY,
     association         VARCHAR(512) NOT NULL,
-    search_association  VARCHAR(512) NOT NULL,
+    search_association  VARCHAR(512) NOT NULL
 );
 
 CREATE INDEX cjdb_associations_sa_exact_idx ON cjdb_associations (search_association);
