1d40df3b6099f280319e685e5753e9f215bfd421
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2007-09-06 21:24:43 +0000

    New DBIx::Class modules

---
+++ lib/CUFTS/Schema/ERMLicense.pm
@@ -0,0 +1,79 @@
+## CUFTS::Schema::ERMLicense
+##
+## Copyright Todd Holbrook, Simon Fraser University (2007)
+##
+## This file is part of CUFTS.
+##
+## CUFTS is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free
+## Software Foundation; either version 2 of the License, or (at your option)
+## any later version.
+## 
+## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along
+## with CUFTS; if not, write to the Free Software Foundation, Inc., 59
+## Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package CUFTS::Schema::ERMLicense;
+
+use strict;
+use base qw/DBIx::Class/;
+
+__PACKAGE__->load_components(qw/PK::Auto Core/);
+
+__PACKAGE__->table('erm_license');
+__PACKAGE__->add_columns( qw(
+    id
+    key
+    site
+
+    full_on_campus_access
+    full_on_campus_notes
+    allows_remote_access
+    allows_proxy_access
+    allows_commercial_use
+    allows_walkins
+    allows_ill
+    ill_notes
+    allows_ereserves
+    ereserves_notes
+    allows_coursepacks
+    coursepack_notes
+    allows_distance_ed
+    allows_downloads
+    allows_prints
+    allows_emails
+    emails_notes
+    allows_archiving
+    archiving_notes
+    own_data
+    citation_requirements
+    requires_print
+    requires_print_plus
+    additional_requirements
+    allowable_downtime
+    online_terms
+    user_restrictions
+    terms_notes
+    termination_requirements
+    perpetual_access
+    perpetual_access_notes
+    contact_name
+    contact_role
+    contact_address
+    contact_phone
+    contact_fax
+    contact_email
+    contact_notes
+));                                                                                                        
+
+__PACKAGE__->set_primary_key( 'id' );
+
+__PACKAGE__->has_many( 'erm_mains' => 'CUFTS::Schema::ERMMain',  'license' );
+
+
+1;
