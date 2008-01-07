bfb86e0f91b3e1791170dcf515ed4a24efcabf75
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2008-01-07 04:13:16 +0000

    Added ISSN validation

--- lib/CUFTS/DB/Stats.pm
+++ lib/CUFTS/DB/Stats.pm
@@ -27,105 +27,112 @@ use base 'CUFTS::DB::DBI';
 __PACKAGE__->table('stats');
 __PACKAGE__->columns(Primary => 'id');
 __PACKAGE__->columns(All => qw(
-	id
+    id
 
-	request_date
-	request_time
+    request_date
+    request_time
 
-	site
+    site
 
-	issn
-	isbn
-	title
-	volume
-	issue
-	date
-	doi
-	results
+    issn
+    isbn
+    title
+    volume
+    issue
+    date
+    doi
+    results
 ));                                                                                                        
 
 __PACKAGE__->sequence('stats_id_seq');
 
 sub normalize_column_values {
-	my ($self, $values) = @_;
-	
-	# Check ISSNs for dashes and strip them out
+    my ($self, $values) = @_;
+    
+    # Check ISSNs for dashes and strip them out
 
-	if ( exists($values->{title}) && defined($values->{title}) ) {
-	    $values->{title} = substr( $values->{title}, 0, 512 );
-	}
-	
-	return 1;   # ???
+    if ( exists($values->{title}) && defined($values->{title}) ) {
+        $values->{title} = substr( $values->{title}, 0, 512 );
+    }
+    
+    if (exists($values->{issn}) && defined($values->{issn}) && $values->{issn} ne '') {
+        $values->{issn} = uc($values->{issn});
+        if ( $values->{issn} !~ s/(\d{4})\-?(\d{3}[\dxX])/$1$2/ ) {
+            warn("Invalid ISSN skipped in statistics.")
+        }
+    }
+    
+    return 1;   # ???
 }
 
 
 __PACKAGE__->set_sql('top50journals' => qq{
-	SELECT issn, title, COUNT(*) as requests FROM
-	__TABLE__
-	WHERE request_date > (current_date - interval '%s')
-	AND site = ?
-	AND results = ?
-	GROUP BY issn, title 
-	ORDER BY COUNT(*) DESC
-	LIMIT 50
+    SELECT issn, title, COUNT(*) as requests FROM
+    __TABLE__
+    WHERE request_date > (current_date - interval '%s')
+    AND site = ?
+    AND results = ?
+    GROUP BY issn, title 
+    ORDER BY COUNT(*) DESC
+    LIMIT 50
 });
 
 __PACKAGE__->set_sql('requests_count' => qq{
-	SELECT COUNT(*) FROM
-	__TABLE__
-	WHERE request_date > (current_date - interval '%s')
-	AND site = ?
+    SELECT COUNT(*) FROM
+    __TABLE__
+    WHERE request_date > (current_date - interval '%s')
+    AND site = ?
 });
 
 __PACKAGE__->set_sql('requests_count_fulltext' => qq{
-	SELECT COUNT(*) FROM
-	__TABLE__
-	WHERE request_date > (current_date - interval '%s')
-	AND site = ?
-	AND results = 't'
+    SELECT COUNT(*) FROM
+    __TABLE__
+    WHERE request_date > (current_date - interval '%s')
+    AND site = ?
+    AND results = 't'
 });
 
 
 
 
 sub top50journals {
-	my ($class, $site, $time, $results) = @_;
-	
-	my $sth = $class->sql_top50journals(_internal_time($time));
-	$sth->execute($site, $results);
-	my $records = $sth->fetchall_arrayref;
+    my ($class, $site, $time, $results) = @_;
+    
+    my $sth = $class->sql_top50journals(_internal_time($time));
+    $sth->execute($site, $results);
+    my $records = $sth->fetchall_arrayref;
 
-	return $records;
+    return $records;
 }
 
 sub requests_count {
-	my ($class, $site, $time) = @_;
-	
-	my %results;
+    my ($class, $site, $time) = @_;
+    
+    my %results;
 
-	my $sth = $class->sql_requests_count(_internal_time($time));
-	$sth->execute($site);
-	my @row = $sth->fetchrow_array;
-	$results{'total_requests'} = $row[0];
+    my $sth = $class->sql_requests_count(_internal_time($time));
+    $sth->execute($site);
+    my @row = $sth->fetchrow_array;
+    $results{'total_requests'} = $row[0];
 
-	$sth = $class->sql_requests_count_fulltext(_internal_time($time));
-	$sth->execute($site);
-	@row = $sth->fetchrow_array;
-	$results{'requests_with_fulltext'} = $row[0];
+    $sth = $class->sql_requests_count_fulltext(_internal_time($time));
+    $sth->execute($site);
+    @row = $sth->fetchrow_array;
+    $results{'requests_with_fulltext'} = $row[0];
 
-	return \%results;
+    return \%results;
 }
 
 
 sub _internal_time {
-	$_[0] eq 'last_week' and
-		return '1 week';
-	$_[0] eq 'last_month' and
-		return '1 month';
-	$_[0] eq 'last_year' and
-		return '1 year';
+    $_[0] eq 'last_week' and
+        return '1 week';
+    $_[0] eq 'last_month' and
+        return '1 month';
+    $_[0] eq 'last_year' and
+        return '1 year';
 
-	CUFTS::Exception->throw("Unrecognized time period: $_[0]");
+    CUFTS::Exception->throw("Unrecognized time period: $_[0]");
 }
 
 
