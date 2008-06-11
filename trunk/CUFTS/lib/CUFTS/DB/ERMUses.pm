b30199883d65b5e0e09ca0a431ca54cdcff26e5f
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2008-06-11 00:55:11 +0000

    New count routine for stats

--- trunk/CUFTS/lib/CUFTS/DB/ERMUses.pm
+++ trunk/CUFTS/lib/CUFTS/DB/ERMUses.pm
@@ -37,4 +37,43 @@ __PACKAGE__->sequence('erm_uses_id_seq');
 
 __PACKAGE__->has_a('erm_main', 'CUFTS::DB::ERMMain');
 
+
+__PACKAGE__->set_sql('count_grouped_with_name' => qq{
+    SELECT erm_uses.erm_main AS erm_main, erm_names.name AS name, COUNT(*), %s AS date_trunc FROM
+    erm_uses
+    JOIN erm_names ON ( erm_uses.erm_main = erm_names.id )
+    WHERE date BETWEEN ? AND ?
+    AND erm_uses.erm_main IN (%s)
+    AND erm_names.main = 1
+    GROUP BY date_trunc, erm_uses.erm_main, name
+    ORDER BY date_trunc, name
+});
+
+__PACKAGE__->set_sql('count_grouped' => qq{
+    SELECT erm_uses.erm_main AS erm_main, COUNT(*), %s AS date_trunc FROM
+    erm_uses
+    WHERE date BETWEEN ? AND ?
+    AND erm_uses.erm_main IN (%s)
+    GROUP BY date_trunc, erm_uses.erm_main
+    ORDER BY date_trunc
+});
+
+sub count_grouped {
+    my ( $class, $granularity, $start_date, $end_date, $erm_mains ) = @_;
+
+    # Granularity isn't in a bind variable so make sure it's clean.
+    $granularity = (grep { $_ eq $granularity } qw( year month day ))[0]
+        or die("Unrecognized granularity: $granularity");
+
+    my $date_trunc = "DATE_TRUNC('$granularity', date)";
+    my $erm_mains_string = join(',', map { int($_) } @$erm_mains );  # Also not a bind variable, clean with "int()"
+    
+    my $sth = $class->sql_count_grouped($date_trunc, $erm_mains_string);
+    $sth->execute($start_date, $end_date);
+    my $records = $sth->fetchall_arrayref;
+
+    return $records;
+}
+
+
 1;
