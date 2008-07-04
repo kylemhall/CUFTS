0ff30fad972a43f9bfe771bdbcdb368cdca1355a
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2008-07-04 01:25:02 +0000

    whitespace cleanup

--- trunk/CUFTS/CJDB/lib/CUFTS/CJDB/C/Journal.pm
+++ trunk/CUFTS/CJDB/lib/CUFTS/CJDB/C/Journal.pm
@@ -7,25 +7,25 @@ use XML::RAI;
 use LWP::Simple;
 
 sub view : Private {
-	my ($self, $c, $journals_auth_id) = @_;
+    my ($self, $c, $journals_auth_id) = @_;
 
-	defined($journals_auth_id) or
-		die "journal auth id not defined";
-	
-	my $site_id = $c->stash->{current_site}->id;
+    defined($journals_auth_id) or
+        die "journal auth id not defined";
+    
+    my $site_id = $c->stash->{current_site}->id;
 
-	my $journal = CJDB::DB::Journals->search(site => $site_id, journals_auth => $journals_auth_id)->first;
-	defined($journal) or
-		die("Unable to retrieve journal auth id $journals_auth_id");
+    my $journal = CJDB::DB::Journals->search(site => $site_id, journals_auth => $journals_auth_id)->first;
+    defined($journal) or
+        die("Unable to retrieve journal auth id $journals_auth_id");
 
-	if ($c->stash->{current_account}) {
-		my @my_tags =  CJDB::DB::Tags->search(
-			'journals_auth' => $journals_auth_id,
-			'account' => $c->stash->{current_account}->id,
-			{ order_by => 'tag' }
-		);
-		$c->stash->{my_tags} = \@my_tags;
-	}
+    if ($c->stash->{current_account}) {
+        my @my_tags =  CJDB::DB::Tags->search(
+            'journals_auth' => $journals_auth_id,
+            'account' => $c->stash->{current_account}->id,
+            { order_by => 'tag' }
+        );
+        $c->stash->{my_tags} = \@my_tags;
+    }
 
     if ( not_empty_string($journal->rss) ) {
         my $xml = get($journal->rss);
@@ -59,22 +59,22 @@ sub view : Private {
     };
     
 
-	$c->stash->{tags} = CJDB::DB::Tags->get_tag_summary($journals_auth_id, $c->stash->{current_site}->id, (defined($c->stash->{current_account}) ? $c->stash->{current_account}->id : undef));
-	$c->stash->{journal} = $journal;	
-	$c->stash->{template} = 'journal.tt';
+    $c->stash->{tags} = CJDB::DB::Tags->get_tag_summary($journals_auth_id, $c->stash->{current_site}->id, (defined($c->stash->{current_account}) ? $c->stash->{current_account}->id : undef));
+    $c->stash->{journal} = $journal;    
+    $c->stash->{template} = 'journal.tt';
 }
 
 sub rss_proxy : Local {
-	my ($self, $c, $journals_auth_id) = @_;
+    my ($self, $c, $journals_auth_id) = @_;
 
-	defined($journals_auth_id) or
-		die "journal auth id not defined";
-	
-	my $site_id = $c->stash->{current_site}->id;
+    defined($journals_auth_id) or
+        die "journal auth id not defined";
+    
+    my $site_id = $c->stash->{current_site}->id;
 
-	my $journal = CJDB::DB::Journals->search(site => $site_id, journals_auth => $journals_auth_id)->first;
-	defined($journal) or
-		die("Unable to retrieve journal auth id $journals_auth_id");
+    my $journal = CJDB::DB::Journals->search(site => $site_id, journals_auth => $journals_auth_id)->first;
+    defined($journal) or
+        die("Unable to retrieve journal auth id $journals_auth_id");
 
     if ( not_empty_string($journal->rss) ) {
         my $xml = get($journal->rss);
@@ -107,11 +107,11 @@ sub rss_proxy : Local {
 }
 
 sub manage_tags : Local {
-	my ($self, $c, $journals_auth_id) = @_;
-	
-	$c->stash->{'show_manage_tags'} = 1;
-	
-	$c->forward('/journal/view', [$journals_auth_id]);
+    my ($self, $c, $journals_auth_id) = @_;
+    
+    $c->stash->{'show_manage_tags'} = 1;
+    
+    $c->forward('/journal/view', [$journals_auth_id]);
 }
 
 1;
