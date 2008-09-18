60250c0114437d7c0f17a74f48a79a1c9cb95a48
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2008-09-18 03:04:09 +0000

    Force latin-1 encoding on JSON browse in CRDB

--- trunk/CUFTS/CRDB/lib/CUFTS/CRDB/Controller/Browse.pm
+++ trunk/CUFTS/CRDB/lib/CUFTS/CRDB/Controller/Browse.pm
@@ -186,6 +186,8 @@ sub json_facets : Chained('facet_options') PathPart('facets/json') Args {
         @records = sort { $a->{sort_name} cmp $b->{sort_name} } @records;
     }
     
+    $c->response->content_type('application/json; charset=iso-8859-1');
+
     $c->stash->{json}->{records} = \@records;
     $c->stash->{current_view}  = 'JSON';
 }
