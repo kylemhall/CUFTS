0e6b6525a62a5829b2abd8f2bd591b41ac7fe17e
smiri <smiri@61ef7b19-5605-0410-bf70-c150524503b3>
2008-12-19 20:57:33 +0000

    modified parser to work with the new title list.

--- lib/CUFTS/Resources/ElsevierEurope.pm
+++ lib/CUFTS/Resources/ElsevierEurope.pm
@@ -75,21 +75,27 @@ sub title_list_split_row {
     my ( $class, $row ) = @_;
 
     $row =~ s/;+\s*$//;
-    $row =~ s/"{2,3}/"/g;
+    $row =~ s/"",""/","/g;
+    $row =~ s/([^,])"",/$1",/g;
+    $row =~ s/,""([^,])/,"$1/g;
+    $row =~ s/"+\s*$/"/;
+    $row =~ s/([^,])"([^,]*)"([^,])/$1'$2'$3/g;
+
+    $row =~ s/",([^"])/","$1/g;
+    $row =~ s/([^"]),"/$1","/g;
 
     $row =~ s/"?;"?/;/g;
-    $row =~ s/^"Subscribed,"/"Subscribed","/;
-#    $row =~ s/Subscribed,"Journal/Subscribed","Journal/;
+    $row =~ s/^"?Subscribed,"/"Subscribed","/;
 
-#	$row =~ s/"\s*"//g;
-#	$row =~ s/\s*"/"/g;
-#	$row =~ s/[\s\t]+$//;
+    $row =~ s/\s*"/"/g;
+    $row =~ s/"\s*/"/g;
+    $row =~ s/([^",])"([^",])/$1'$2/g;
+    $row =~ s/"""/","/;
 
     my $csv = CUFTS::Util::CSVParse->new();
     $csv->parse($row)
         or CUFTS::Exception::App->throw(
         'Error parsing CSV line: ' . $csv->error_input() );
-
     my @fields = $csv->fields;
     return \@fields;
 }
@@ -97,50 +103,69 @@ sub title_list_split_row {
 sub clean_data {
     my ( $class, $record ) = @_;
 
-#    if ( $record->{ft_start_date} =~ /(\d+) (\w+) (\d+)/xsm ) {
-#	my ( $day, $month, $year ) = ( $1, $2, $3 );
-#print STDERR "$day / $month / $year\n";
-#	$month = get_month( $month, 'start' );
-#	if (!$month){
-#	    delete $record->{ft_start_date};
-#	}elsif (!$day){
-#	    $record->{ft_start_date} = sprintf( "%04d-%02d-00", $year, $month );
-#	}else{
-#	    $record->{ft_start_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
-#	}
-#    }
-    if ( $record->{ft_start_date} =~ / (\d+) - (\w+) - (\d+) /xsm ) {
+    if ( $record->{ft_start_date} =~ /(\d+)?\s*(\w+)\s+(\d+)/xsm ) {
+	my ( $day, $month, $year ) = ( $1, $2, $3 );
+	$month = get_month( $month, 'start' );
+	if (!$month){
+	    delete $record->{ft_start_date};
+	}elsif (!$day){
+	    $record->{ft_start_date} = sprintf( "%04d-%02d-00", $year, $month );
+	}else{
+	    $record->{ft_start_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
+	}
+    }
+    elsif ( $record->{ft_start_date} =~ / (\d+) - (\w+) - (\d+) /xsm ) {
         my ( $day, $month, $year ) = ( $1, $2, $3 );
         $month = get_month( $month, 'start' );
         $record->{ft_start_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
+    }
+    elsif ( $record->{ft_start_date} =~ /(\d{4})/xsm ) {
+        $record->{ft_start_date} = $1;
     }
     else {
         delete $record->{ft_start_date};
     }
 
-    if ( $record->{ft_end_date} =~ / (\d+) - (\w+) - (\d+) /xsm ) {
-
+    if ( $record->{ft_end_date} =~ /(\d+)?\s*(\w+)\s+(\d+)/xsm ) {
         my ( $day, $month, $year ) = ( $1, $2, $3 );
-        
-        # Remove end periods if the year matches the current year
-
-        my $current_year = (localtime)[5] + 1900;
+	my $current_year = (localtime)[5] + 1900;
         if ( int($year) >= $current_year ) {
-            
             delete $record->{iss_ft_end};
             delete $record->{vol_ft_end};
             delete $record->{ft_end_date};
-            
         }
         else {
             $month = get_month( $month, 'end' );
             $record->{ft_end_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
         }
-
+    }
+    elsif ( $record->{ft_end_date} =~ / (\d+) - (\w+) - (\d+) /xsm ) {
+        my ( $day, $month, $year ) = ( $1, $2, $3 );
+        # Remove end periods if the year matches the current year
+        my $current_year = (localtime)[5] + 1900;
+        if ( int($year) >= $current_year ) {
+            delete $record->{iss_ft_end};
+            delete $record->{vol_ft_end};
+            delete $record->{ft_end_date};
+        }
+        else {
+            $month = get_month( $month, 'end' );
+            $record->{ft_end_date} = sprintf( "%04d-%02d-%02d", $year, $month, $day );
+        }
+    }
+    elsif ( $record->{ft_end_date} =~ /(\d{4})/xsm ) {
+	$record->{ft_end_date} = $1;
+	my $current_year = (localtime)[5] + 1900;
+        if ( int($1) >= $current_year ) {
+            delete $record->{iss_ft_end};
+            delete $record->{vol_ft_end};
+            delete $record->{ft_end_date};
+        }
     }
     else {
         delete $record->{ft_end_date};
     }
+
 
     if ( $record->{vol_ft_start} =~ /^ (\d+) - /xsm ) {
         $record->{vol_ft_start} = $1;
