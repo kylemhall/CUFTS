c2dc8376f7e6b424ab5294ab12077e0995141e5e
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2009-05-14 17:07:52 +0000

    Restored linking part of Erudit module

--- trunk/CUFTS/lib/CUFTS/Resources/Erudit.pm
+++ trunk/CUFTS/lib/CUFTS/Resources/Erudit.pm
@@ -76,137 +76,13 @@ sub title_list_split_row {
     return \@fields;
 }
 
-sub clean_data {
-    my ( $class, $record ) = @_;
-    
-    return $class->SUPER::clean_data($record);
-}
-
-## global_resource_details - Controls which details are displayed on the global
-## resource pages
-##
-
-sub global_resource_details {
-    my ($class) = @_;
-    return [
-        @{ $class->SUPER::global_resource_details },
-        qw(
-            resource_identifier
-        )
-    ];
-}
-
-sub local_resource_details {
-    my ($class) = @_;
-    return [
-        @{ $class->SUPER::local_resource_details },
-        qw(
-            auth_name
-        )
-    ];
-}
-
-## help_template - path to the help template for this resource relative to the
-## general templates directory
-##
-
-sub help_template {
-    return 'help/Wilson';
-}
-
-## resource_details_help - A hash ref containing the hoverover help for each of the
-## local resource details
-##
-
-sub resource_details_help {
-    return {
-        'resource_identifier' =>
-            'This is a code that Wilson uses to uniquely identify '
-            . "each database. \n\nExample: BAIN is the resource identifier for "
-            . 'Biological and Agricultural Index Plus',
-
-        'auth_name' =>
-            'If there is a need to disambiguate overlapping TCP/IP ranges for sites '
-            . 'that may be members of multiple customer accounts in the authentication database, use '
-            . 'if no other automatic login method is enabled, can be used to supply the '
-            . "login username, in conjunction with the authorize password field below.\n\n"
-            . 'Caution: values appear as plain text in browser address bar.'
-    };
-}
-
-## -------------------------------------------------------------------------------------------
-
-## can_get* - Control whether or not an attempt to create a link is built.  This is run
-## before the database is searched for possible title matches, so catching requests without
-## enough data, etc. early (here) cuts down on database hits
-
-sub can_getFulltext {
-    my ( $class, $request ) = @_;
-
-    return 0 if is_empty_string( $request->spage  )
-             && is_empty_string( $request->atitle );
-             
-    return $class->SUPER::can_getFulltext($request);
-}
-
 sub can_getTOC {
     my ( $class, $request ) = @_;
 
-    return 0 if is_empty_string( $request->volume );
-    
+    return 0 unless defined( $request->issue ) && $request->issue =~ /^\d+$/;
+    return 0 unless defined( $request->year )  && $request->year  =~ /^\d{4}$/;
+
     return $class->SUPER::can_getTOC($request);
-}
-
-# --------------------------------------------------------------------------------------------
-
-## build_link* - Builds a link to a service.  Should return an array reference containing
-## Result objects with urls and title list records (if applicable).
-##
-
-sub build_linkFulltext {
-    my ( $class, $records, $resource, $site, $request ) = @_;
-
-    defined($records) && scalar(@$records) > 0
-        or return [];
-    defined($resource)
-        or CUFTS::Exception::App->throw('No resource defined in build_linkFulltext');
-    defined($site)
-        or CUFTS::Exception::App->throw('No site defined in build_linkFulltext');
-    defined($request)
-        or CUFTS::Exception::App->throw('No request defined in build_linkFulltext');
-
-    my @results;
-
-    foreach my $record (@$records) {
-
-        my $url = 'http://vnweb.hwwilsonweb.com/hww/jumpstart.jhtml?';
-
-        # if username provided, use it in the url
-        if ( not_empty_string( $resource->auth_name ) ) {
-            $url .= 'CustID=' . $resource->auth_name . '&';
-        }
-
-        $url .= 'genre=article&sid=HWW:' . $resource->resource_identifier;
-        $url .= not_empty_string( $record->issn )
-                ? '&issn='  . dashed_issn( $record->issn )
-                : '&title=' . uri_escape( $record->title );
-
-        if ( $request->spage ) {
-            $url .= '&volume=' . $request->volume
-                  . '&issue='  . $request->issue
-                  . '&spage='  . $request->spage;
-        }
-        else {
-            $url .= '&atitle=' . uri_escape( $request->atitle );
-        }
-
-        my $result = new CUFTS::Result($url);
-        $result->record($record);
-
-        push @results, $result;
-    }
-
-    return \@results;
 }
 
 sub build_linkTOC {
@@ -224,24 +100,13 @@ sub build_linkTOC {
     my @results;
 
     foreach my $record (@$records) {
+        next if is_empty_string( $record->journal_url );
 
-        my $url = 'http://vnweb.hwwilsonweb.com/hww/jumpstart.jhtml?';
-
-        # if username provided, use it in the url
-        if ( not_empty_string( $resource->auth_name) ) {
-            $url .= 'CustID=' . $resource->auth_name . '&';
-        }
-
-        $url .= 'genre=journal&sid=HWW:' . $resource->resource_identifier;
-
-        $url .= not_empty_string( $record->issn )
-                ? '&issn='  . dashed_issn( $record->issn )
-                : '&title=' . uri_escape( $record->title );
-
-        $url .= '&volume=' . $request->volume;
-        if ( not_empty_string( $request->issue ) ) {
-            $url .= '&issue='  . $request->issue if $request->issue;
-        }
+        my $url = $record->journal_url;
+        $url .= '/'  . $request->year;
+        $url .= '/v' . $request->volume;
+        $url .= '/n' . $request->issue;
+        $url .= '/index.html';
 
         my $result = new CUFTS::Result($url);
         $result->record($record);
@@ -267,26 +132,16 @@ sub build_linkJournal {
     my @results;
 
     foreach my $record (@$records) {
+        next if is_empty_string( $record->journal_url );
 
-	my $url = 'http://vnweb.hwwilsonweb.com/hww/Journals/searchAction.jhtml?';
+        my $result = new CUFTS::Result( $record->journal_url );
+        $result->record($record);
 
-        # if username provided, use it in the url
-        if ( not_empty_string( $resource->auth_name) ) {
-            $url .= 'CustID=' . $resource->auth_name . '&';
-        }
-
-	$url .= 'sid=HWW:' . $resource->resource_identifier;
-	$url .= not_empty_string( $record->issn )
-		? '&issn='  . dashed_issn( $record->issn )
-		: '&title=' . uri_escape( $record->title );
-
-	my $result = new CUFTS::Result($url);
-	$result->record($record);
-
-	push @results, $result;
+        push @results, $result;
     }
 
     return \@results;
 }
+
 
 1;
