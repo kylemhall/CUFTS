668e92c862720262b5beb05f77f882f81e59dd1a
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2009-09-14 21:04:02 +0000

    Fixed incorrect column name in erm_sushi SQL

--- trunk/CUFTS/sql/CUFTS/erm_sushi.sql
+++ trunk/CUFTS/sql/CUFTS/erm_sushi.sql
@@ -2,7 +2,7 @@ CREATE TABLE erm_sushi (
     id              SERIAL PRIMARY KEY,
     site            INTEGER NOT NULL,
     name            VARCHAR(255) NOT NULL,
-    reference       VARCHAR(255),
+    requestor       VARCHAR(255),
     service_url     VARCHAR(255),
     interval_months INTEGER
 );
