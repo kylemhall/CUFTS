6bae73974de42f3e86421bf73abd72b0c0407700
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2010-06-07 16:12:05 +0000

    Removed "new" call that was brought over from Class::DBI.  Should be implemented differently for DBIC

--- lib/CUFTS/Schema/ERMCounterCounts.pm
+++ lib/CUFTS/Schema/ERMCounterCounts.pm
@@ -25,7 +25,7 @@ use base qw/DBIx::Class/;
 
 use Date::Calc qw(Days_in_Month);
 
-__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->load_components(qw/TimeStamp Core/);
 
 __PACKAGE__->table('erm_counter_counts');
 __PACKAGE__->add_columns(
@@ -65,8 +65,8 @@ __PACKAGE__->add_columns(
         default_value => 0,
     },
     timestamp => {
-        data_type => 'timestamp',
-        default_value => \'NOW()',
+        data_type => 'datetime',
+        set_on_create => 1,
     },
 );                                                                                               
 
@@ -74,17 +74,17 @@ __PACKAGE__->set_primary_key( 'id' );
 
 __PACKAGE__->belongs_to( 'counter_source' => 'CUFTS::Schema::ERMCounterSources' );
 
-sub new {
-    my ($self, $attrs) = @_;
-    
-    # Set the end date if it isn't already set.
-
-    if ( !exists($attrs->{end_date}) && defined($attrs->{start_date}) && $attrs->{start_date} =~ /(\d{4})-(\d{2})-\d{2}/ ) {
-        $attrs->{end_date} = "$1-$2-" . Days_in_Month($1,$2);
-    }
-
-    return 1;   # ???
-}
+# sub new {
+#     my ($self, $attrs) = @_;
+#     
+#     # Set the end date if it isn't already set.
+# 
+#     if ( !exists($attrs->{end_date}) && defined($attrs->{start_date}) && $attrs->{start_date} =~ /(\d{4})-(\d{2})-\d{2}/ ) {
+#         $attrs->{end_date} = "$1-$2-" . Days_in_Month($1,$2);
+#     }
+# 
+#     return 1;   # ???
+# }
 
 
 1;
