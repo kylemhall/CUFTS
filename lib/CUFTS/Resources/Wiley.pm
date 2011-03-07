36c394f065740fd8588837c8f6b525675a24a7c3
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2011-03-07 18:16:17 +0000

    Changed Wiley fulltext linking to send DOIs directly to Wiley instead of going through Crossref. Some journals had changed publishers and Crossref was sending them to the wrong palce.

--- lib/CUFTS/Resources/Wiley.pm
+++ lib/CUFTS/Resources/Wiley.pm
@@ -20,7 +20,7 @@
 
 package CUFTS::Resources::Wiley;
 
-use base qw(CUFTS::Resources::GenericJournalDOI);
+use base qw(CUFTS::Resources::GenericJournal);
 
 use CUFTS::Exceptions;
 use CUFTS::Util::Simple;
@@ -100,5 +100,40 @@ sub clean_data {
 
     return $class->SUPER::clean_data($record);
 }
+
+sub build_linkFulltext {
+    my ($class, $records, $resource, $site, $request) = @_;
+
+    defined($records) && scalar(@$records) > 0 or 
+        return [];
+    defined($resource) or 
+        CUFTS::Exception::App->throw('No resource defined in build_linkFulltext');
+    defined($site) or 
+        CUFTS::Exception::App->throw('No site defined in build_linkFulltext');
+    defined($request) or 
+        CUFTS::Exception::App->throw('No request defined in build_linkFulltext');
+
+    if ( not_empty_string($request->doi) ) {
+        my $url;
+        $url .= 'http://onlinelibrary.wiley.com/doi/';
+        $url .= uri_escape($request->doi, "^A-Za-z0-9\-_.!~*'()\/");
+        $url .= '/abstract';
+
+        my $result = new CUFTS::Result($url);
+        $result->record($records->[0]);
+        
+        return [$result];
+    } else {
+        return [];
+    }
+}
+
+sub can_getFulltext {
+    my ($class, $request) = @_;
+
+    return 1 if not_empty_string($request->doi);
+    return 0;   
+}
+
 
 1;
