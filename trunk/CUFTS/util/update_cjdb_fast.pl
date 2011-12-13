855c7246ee29af46207cc96139e168a0ce33ffa4
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2011-12-13 21:10:24 +0000

    Better logging of resources as they're being processed - some resources are skipped (deactivated global resources, for example) but weren't being reported so the counts were strange.

--- trunk/CUFTS/util/update_cjdb_fast.pl
+++ trunk/CUFTS/util/update_cjdb_fast.pl
@@ -256,12 +256,20 @@ sub load_cufts_links {
 
     foreach my $local_resource (@local_resources) {
         $resource_count++;
-        next if defined($local_resource->resource) && !$local_resource->resource->active;  # Skip deactivated global resources
+        $logger->info("Processing resource: $resource_count/", scalar(@local_resources), ': ', $resource->name);
+        
+        # Skip deactivated global resources
+        if ( defined($local_resource->resource) && !$local_resource->resource->active ) {
+            $logger->info('Skipping deactivated resource');
+            next;
+        }
 
         my $resource = CUFTS::Resolve->overlay_global_resource_data($local_resource);
-        next if !defined( $resource->module );
+        if ( !defined( $resource->module ) ) {
+            $logger->info('Skipping resource with no defined module');
+            next;
+        }
 
-        $logger->info("Processing resource: $resource_count/", scalar(@local_resources), ': ', $resource->name);
         my $resource_id   = $resource->id;
         my $resource_rank = $resource->rank;
         my $module = CUFTS::Resolve::__module_name( $resource->module );
