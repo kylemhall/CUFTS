3163a930bf9c86e9fd6c95589b171a97d4f221cc
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2012-05-16 21:36:35 +0000

    Removed old CJDB delete script. It was dangerous and not needed as is.

--- trunk/CUFTS/util/delete_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-
-echo 'delete from cjdb_subjects' | psql $1
-echo 'delete from cjdb_links' | psql $1
-echo 'delete from cjdb_journals' | psql $1
-echo 'delete from cjdb_associations' | psql $1
-echo 'delete from cjdb_titles' | psql $1
-echo 'delete from cjdb_issns' | psql $1
