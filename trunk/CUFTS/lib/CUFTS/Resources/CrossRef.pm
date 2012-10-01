5d4f785eb0d19dfc5071b35b71c03df698a2e512
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2012-10-01 20:52:09 +0000

    Updated to use new Crossref XML interface

--- trunk/CUFTS/lib/CUFTS/Resources/CrossRef.pm
+++ trunk/CUFTS/lib/CUFTS/Resources/CrossRef.pm
@@ -29,6 +29,13 @@ use CUFTS::DB::SearchCache;
 use LWP::UserAgent;
 use HTTP::Request::Common;
 
+use URI::Escape qw(uri_escape);
+use String::Util qw(hascontent);
+use XML::LibXML;
+
+use Data::Dumper;
+
+
 use strict;
 
 sub has_title_list { return 0; }
@@ -49,10 +56,10 @@ sub resource_details_help {
 sub get_records {
     my ( $class, $resource, $site, $request ) = @_;
 
-    if ( is_empty_string( $resource->auth_name ) ) {
+    if ( !hascontent( $resource->auth_name ) ) {
         CUFTS::Exception::App->throw('No auth_name defined for CrossRef lookups.');
     }
-    if ( is_empty_string( $resource->auth_passwd ) ) {
+    if ( !hascontent( $resource->auth_passwd ) ) {
         CUFTS::Exception::App->throw('No auth_passwd defined for CrossRef lookups.');
     }
 
@@ -63,46 +70,33 @@ sub get_records {
 
     my ( $qdata, $qtype, $cache_query );
 
-    if (   not_empty_string( $request->issn )
-        || not_empty_string( $request->title ) )
-    {
-        $cache_query = $qdata = join(
-            '|',
-            (   'q',
-                $request->issn   || '',
-                $request->title  || '',
-                $request->aulast || '',
-                $request->volume || '',
-                $request->issue  || '',
-                $request->spage  || '',
-                $year,
-                $request->doi || '',
-            )
-        );
-
-        $qdata = join(
-            '|',
-            (   $request->issn   || '',
-                $request->title  || '',
-                $request->aulast || '',
-                $request->volume || '',
-                $request->issue  || '',
-                $request->spage  || '',
-                $year,
-                '',
-                time,
-                $request->doi || '',
-            )
-        );
-        $qtype = 'q';
-
+    if ( hascontent( $request->doi ) ) {
+        $cache_query = 'http://www.crossref.org/openurl/?noredirect=true&id=' . uri_escape($request->doi);
     }
-    elsif ( not_empty_string( $request->doi ) ) {
+    elsif ( hascontent( $request->issn ) || hascontent( $request->eissn ) || hascontent( $request->title ) ) {
+        $cache_query = 'http://www.crossref.org/openurl/?noredirect=true';
 
-        $qdata       = $request->doi;
-        $qtype       = 'd';
-        $cache_query = 'd|' . $request->doi;
-
+        if ( hascontent($request->issn) ) {
+            $cache_query .= '&issn=' . uri_escape($request->issn);
+        }
+        if ( hascontent($request->eissn) ) {
+            $cache_query .= '&eissn=' . uri_escape($request->eissn);
+        }
+        if ( hascontent($request->title) ) {
+            $cache_query .= '&title=' . uri_escape($request->title);
+        }
+        if ( hascontent($request->volume) ) {
+            $cache_query .= '&volume=' . uri_escape($request->volume);
+        }
+        if ( hascontent($request->issue) ) {
+            $cache_query .= '&issue=' . uri_escape($request->issue);
+        }
+        if ( hascontent($request->spage) ) {
+            $cache_query .= '&spage=' . uri_escape($request->spage);
+        }
+        if ( hascontent($request->date) ) {
+            $cache_query .= '&date=' . uri_escape($request->date);
+        }
     }
     else {
         return undef;
@@ -112,74 +106,83 @@ sub get_records {
 
     my $cache_data = CUFTS::DB::SearchCache->search(
         type    => 'crossref',
-        'query' => $cache_query
+        'query' => $cache_query,
     )->first;
 
     if ( !defined($cache_data) ) {
 
-        # Lookup meta-data
+        # Add username/password to OpenURL
+
+        my $openurl = $cache_query . '&pid=' . uri_escape($resource->auth_name . ':' . $resource->auth_passwd);
+
+        # Lookup metadata
 
         my $start_time = time;
 
         my $ua = LWP::UserAgent->new( 'timeout' => 20 );
-        my $response = $ua->request(
-            POST 'http://doi.crossref.org/servlet/query',
-            [   'type'   => $qtype,
-                'usr'    => $resource->auth_name,
-                'pwd'    => $resource->auth_passwd,
-                'area'   => 'live',
-                'format' => 'piped',
-                'qdata'  => $qdata,
-            ]
-        );
+        my $response = $ua->request( GET $openurl );
 
         $response->is_success or return undef;
-
-        my $returned_data = trim_string( $response->content );
-
-        print STDERR "CrossRef returned ("
-            . ( time - $start_time )
-            . "s): $returned_data\n";
+        my $xml  = trim_string( $response->content );
 
         $cache_data = CUFTS::DB::SearchCache->create(
             {   type   => 'crossref',
                 query  => $cache_query,
-                result => $returned_data,
+                result => $xml,
             }
         );
         CUFTS::DB::SearchCache->dbi_commit;
     }
 
-    my ($issn,  $title,      $aulast,        $volume,
-        $issue, $start_page, $crossref_year, $type,
-        $key,   $doi,
-        )
-        = split /\|/, $cache_data->result;
+    my $doc = XML::LibXML->load_xml( string => $cache_data->result );
+    my $xpc = XML::LibXML::XPathContext->new( $doc->documentElement() );
+    $xpc->registerNs('cr', 'http://www.crossref.org/qrschema/2.0');
 
-    $doi =~ s/\n.+$//msx;  # Remove everything after the first DOI
+
+    my $query = $xpc->findnodes('//cr:query[1]')->shift;
+    return undef if !defined($query);
+
+    my $title  = get_text_if_defined( $xpc, '//cr:query[1]/cr:journal_title');
+    my $atitle = get_text_if_defined( $xpc, '//cr:query[1]/cr:article_title');
+    my $volume = get_text_if_defined( $xpc, '//cr:query[1]/cr:volume');
+    my $issue  = get_text_if_defined( $xpc, '//cr:query[1]/cr:issue');
+    my $spage  = get_text_if_defined( $xpc, '//cr:query[1]/cr:first_page');
+    my $year   = get_text_if_defined( $xpc, '//cr:query[1]/cr:year');
+
+    my $doi    = get_text_if_defined( $xpc, '//cr:query[1]/cr:doi[@type=\'journal_article\']');
+    my $issn   = get_text_if_defined( $xpc, '//cr:query[1]/cr:doi[@type=\'print\']');
+    my $eissn  = get_text_if_defined( $xpc, '//cr:query[1]/cr:doi[@type=\'electronic\']');
     
-    is_empty_string( $request->doi ) && not_empty_string($doi)
+    !hascontent( $request->doi ) && hascontent($doi)
         and $request->doi($doi);
 
-    is_empty_string( $request->aulast ) && not_empty_string($aulast)
-        and $request->aulast($aulast);
+    # !hascontent( $request->aulast ) && hascontent($aulast)
+    #     and $request->aulast($aulast);
 
-    is_empty_string( $request->title ) && not_empty_string($title)
+    !hascontent( $request->title ) && hascontent($title)
         and $request->title($title);
 
-    if ( is_empty_string( $request->issn ) && not_empty_string($issn) ) {
+    !hascontent( $request->atitle ) && hascontent($atitle)
+        and $request->atitle($atitle);
+
+    if ( !hascontent( $request->issn ) && hascontent($issn) ) {
         $issn =~ /^([\dxX]{8})/
             and $request->issn($1);
     }
 
-    is_empty_string( $request->volume ) && not_empty_string($volume)
+    if ( !hascontent( $request->eissn ) && hascontent($eissn) ) {
+        $eissn =~ /^([\dxX]{8})/
+            and $request->eissn($1);
+    }
+
+    !hascontent( $request->volume ) && hascontent($volume)
         and $request->volume($volume);
 
-    is_empty_string( $request->issue ) && not_empty_string($issue)
+    !hascontent( $request->issue ) && hascontent($issue)
         and $request->issue($issue);
 
-    is_empty_string( $request->spage ) && not_empty_string($start_page)
-        and $request->spage($start_page);
+    !hascontent( $request->spage ) && hascontent($spage)
+        and $request->spage($spage);
 
     return undef;
 }
@@ -187,16 +190,24 @@ sub get_records {
 sub can_getMetadata {
     my ( $class, $request ) = @_;
 
-    not_empty_string( $request->issn ) || not_empty_string( $request->eissn )
+    hascontent( $request->issn ) || hascontent( $request->eissn )
         and return 1;
 
-    not_empty_string( $request->title )
+    hascontent( $request->title )
         and return 1;
 
-    not_empty_string( $request->doi )
+    hascontent( $request->doi )
         and return 1;
 
     return 0;
+}
+
+sub get_text_if_defined {
+    my $xpc   = shift;
+    my $xpath = shift;
+
+    my $node = $xpc->findnodes($xpath)->shift;
+    return defined($node) ? $node->textContent : undef;
 }
 
 1;
