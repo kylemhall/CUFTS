2124da1e76188a94a45a327bdd659a2c5cbdaff8
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2012-11-30 19:40:13 +0000

    Added a dash to the ISSN for linking to Metapress / SpringerLink and switched to using the issn OpenURL field.

--- lib/CUFTS/Resources/MetaPress.pm
+++ lib/CUFTS/Resources/MetaPress.pm
@@ -318,10 +318,10 @@ sub build_linkFulltext {
         my $url = $resource->url_base;
         $url .= '?genre=article';
         if ( not_empty_string( $record->e_issn ) ) {
-            $url .= '&eissn=' . $record->e_issn;
+            $url .= '&issn=' . dashed_issn($record->e_issn);
         }
         else {
-            $url .= '&issn=' . $record->issn;
+            $url .= '&issn=' . dashed_issn($record->issn);
         }
 
         if ( not_empty_string( $request->volume ) ) {
