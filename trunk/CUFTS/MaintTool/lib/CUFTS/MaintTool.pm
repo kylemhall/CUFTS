5229bb80767bfd582459637b68ce91285638bf7e
tholbroo <tholbroo@61ef7b19-5605-0410-bf70-c150524503b3>
2013-12-05 18:12:46 +0000

    Add menu items for new template screens

--- trunk/CUFTS/MaintTool/lib/CUFTS/MaintTool.pm
+++ trunk/CUFTS/MaintTool/lib/CUFTS/MaintTool.pm
@@ -56,7 +56,6 @@ sub begin : Private {
         }
     }
     elsif ( $c->req->action ne 'login' ) {
-
         # If we have no current user then show the login screen
 
         $c->req->action(undef);
